50ea653a5207d631a654831353b62bd77de14c67
Paulo Khayat <paulokhayat@gmail.com>
2018-10-18 19:08:08 +0300

    Add rotate function
    Rotates a point around another point

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/Point.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/Point.java
@@ -2,6 +2,7 @@ package robot.subsystems.drivetrain.pure_pursuit;
 
 /**
  * A generic point class for storing (x, y) coordinates
+ *
  * @author Paulo Khayat
  */
 public class Point {
@@ -9,10 +10,11 @@ public class Point {
 
     /**
      * Gets the two coordinates of the point
+     *
      * @param x: the x coordinate of the point
      * @param y: the y coordinate of the point
      */
-    public Point(double x, double y){
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
@@ -50,17 +52,31 @@ public class Point {
      * @param p2 The second Point object reference
      * @return returns the distance between the two points
      */
-    public static double distance(Point p1, Point p2){
-        return Math.hypot(p2.getX()-p1.getX(), p2.getY() - p1.getY());
+    public static double distance(Point p1, Point p2) {
+        return Math.hypot(p2.getX() - p1.getX(), p2.getY() - p1.getY());
     }
 
     /**
-     *
      * @param p1 The first Point object reference
      * @param p2 The second Point object reference
      * @return returns a new Point which is the average of the two given points
      */
-    public static Point average(Point p1, Point p2){
-        return new Point(0.5*(p1.getX()+p2.getX()), 0.5*(p1.getY()+p2.getY()));
+    public static Point average(Point p1, Point p2) {
+        return new Point(0.5 * (p1.getX() + p2.getX()), 0.5 * (p1.getY() + p2.getY()));
+    }
+
+    /**
+     * Rotate a point around a center, in degrees
+     * @param center the center point object
+     * @param p the turning point
+     * @param degrees the degrees to turn the point (clockwise positive)
+     * @return returns a new Point which is the rotated point
+     */
+    public static Point rotate(Point center, Point p, double degrees) {
+        double radians = -Math.toRadians(degrees);
+        double newX = center.getX() + (p.getX()-center.getX())*Math.cos(radians) - (p.getY()-center.getY())*Math.sin(radians);
+
+        double newY = center.getY() + (p.getX()-center.getX())*Math.sin(radians) + (p.getY()-center.getY())*Math.cos(radians);
+        return new Point(newX, newY);
     }
 }
