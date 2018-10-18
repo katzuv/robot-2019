4f517f01643f889912f24d6922e1f099a4f2148a
Unknown <lior5858@gmail.com>
2018-10-18 18:14:54 +0300

    Fix package, Add victors for left and right

--- src/main/java/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/robot/subsystems/drivetrain/Drivetrain.java
@@ -5,20 +5,29 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package robot.drivetrain;
+package robot.subsystems.drivetrain;
 
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  * Add your docs here.
  */
-public class Drivetrain extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
 
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
+
+public class Drivetrain extends Subsystem {
+    VictorSPX leftForward = new VictorSPX(-1);
+    VictorSPX leftBack = new VictorSPX(-1);
+    VictorSPX rightForward = new VictorSPX(-1);
+    VictorSPX rightBack = new VictorSPX(-1);
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
 }
