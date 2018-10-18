a477f11a35ac85fc4facf3aeedd7314d7a1673ea
Unknown <lior5858@gmail.com>
2018-10-18 18:56:49 +0300

    Fix encoders name

--- src/main/java/robot/subsystems/drivetrain/Ports.java
+++ src/main/java/robot/subsystems/drivetrain/Ports.java
@@ -5,9 +5,9 @@ public class Ports {
     final static int leftBackMotor = -1;
     final static int rightForwardMotor = -1;
     final static int rightBackMotor = -1;
-    final static int rightEncderChannelA = -2;
+    final static int rightEncoderChannelA = -2;
     final static int rightEncoderChannelB = -2;
-    final static int leftEncderChannelA = -2;
+    final static int leftEncoderChannelA = -2;
     final static int leftEncoderChannelB = -2;
 
 }
