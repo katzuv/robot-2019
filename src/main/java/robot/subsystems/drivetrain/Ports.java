81b249f4ae114a8c47013ff7fb0c357932c86b17
Unknown <lior5858@gmail.com>
2018-10-18 18:20:28 +0300

    Add encoders ports

--- src/main/java/robot/subsystems/drivetrain/Ports.java
+++ src/main/java/robot/subsystems/drivetrain/Ports.java
@@ -5,4 +5,9 @@ public class Ports {
     final int leftBackMotor = -1;
     final int rightForwardMotor = -1;
     final int rightBackMotor = -1;
+    final int rightEncderChannelA = -2;
+    final int rightEncoderChannelB = -2;
+    final int leftEncderChannelA = -2;
+    final int leftEncoderChannelB = -2;
+
 }
