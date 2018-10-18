1419c2a8fb4c53172af477af99159998db6eeb9a
Unknown <lior5858@gmail.com>
2018-10-18 18:15:05 +0300

    Add ports to motors

--- src/main/java/robot/subsystems/drivetrain/Ports.java
+++ src/main/java/robot/subsystems/drivetrain/Ports.java
@@ -1,4 +1,8 @@
-package robot.drivetrain;
+package robot.subsystems.drivetrain;
 
 public class Ports {
+    final int leftForwardMotor = -1;
+    final int leftBackMotor = -1;
+    final int rightForwardMotor = -1;
+    final int rightBackMotor = -1;
 }
