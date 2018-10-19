e80c915fdacb3a8a422d3d8787887526adecc483
Lior Barkai <lior5858@gmail.com>
2018-10-19 13:51:12 +0300

    Add spacing to constants

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -2,6 +2,7 @@ package robot.subsystems.drivetrain;
 
 class Constants {
     static final double PULSE_PER_DISTANCE = 0.003451;
+    static final double SPACING_BETWEEN_WAYPOINTS = 0.1524; //meters
 
 
 }
