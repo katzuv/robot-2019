69338fd94d5f162699a95e5a5a5864b71704c148
Paulo Khayat <paulokhayat@gmail.com>
2018-10-19 12:34:14 +0300

    Add comments

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/Path.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/Path.java
@@ -1,29 +1,58 @@
 package robot.subsystems.drivetrain.pure_pursuit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
- *
+ * @author Paulo Khayat
+ * @author Lior Barkai
+ * This class is the instance of the path, holding the points.
  */
 public class Path {
-    private ArrayList path = new ArrayList();
+    private ArrayList<Point> path = new ArrayList<Point>();
 
     /**
-     *
+     * Create an empty Path instance
+     */
+    public Path(){
+
+    }
+
+    /**
+     * Create a Path instance
+     * @param array the array of points to add into the arraylist
+     */
+    public Path(Point[] array){
+        path.addAll(Arrays.asList(array));
+    }
+    /**
+     * Adds all of a Point array to the end of the path list
+     * @param array the array of points to add to the end of the array.
      */
     public void addAll(Point[] array){
-        for(int i = 0; i<array.length; i++)
-            path.add(array[i]);
+        path.addAll(Arrays.asList(array));
     }
 
+    /**
+     * Appends all of a Point array at a certain index.
+     * Adds the first Point at the specified index.
+     * all Points at an index greater than the specified index get moved to after the array.
+     * @param index the index to add the Point array (0 if you want the point array to be first in line.
+     * @param array the array of points to add.
+     */
     public void addAll(int index, Point[] array){
-        for(int i = 0; i<array.length; i++)
-            path.add(index+i,array[i]);
+        path.addAll(index, Arrays.asList(array));
     }
 
+    /**
+     * get a Point at a specific index.
+     * @param index index of the point starting at zero.
+     * @return returns the Point.
+     */
     public Point get(int index){
-        if(!(path.get(index) instanceof Point))
+        if(path.get(index) == null)
             throw new ClassCastException("Tried to call a non Point object from the path list.");
-        return (Point) path.get(index);
+        return path.get(index);
     }
 
 
