92567dd5eefe6742b1663f10f7b9bb29a0860cea
Lior Barkai <lior5858@gmail.com>
2018-10-19 11:09:22 +0300

    Implement joystick drive

--- src/main/java/robot/subsystems/drivetrain/commands/JoystickDrive.java
+++ src/main/java/robot/subsystems/drivetrain/commands/JoystickDrive.java
@@ -8,38 +8,43 @@
 package robot.subsystems.drivetrain.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+
+import static robot.Robot.drivetrain;
 
 public class JoystickDrive extends Command {
-  public JoystickDrive() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
-  }
+    public JoystickDrive() {
+        requires(drivetrain);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
 
-  }
+    }
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-  }
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        drivetrain.setSpeed(Robot.m_oi.leftStick.getY(), Robot.m_oi.rightStick.getY());
+    }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-  }
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+    }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-  }
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+    }
 }
