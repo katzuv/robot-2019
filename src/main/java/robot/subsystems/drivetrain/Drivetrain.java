337d60f697a1d628a0c59f0a8c134a2df7800e8c
Lior Barkai <lior5858@gmail.com>
2018-10-19 10:25:55 +0300

    Add setDistancePerPulse for encoders

--- src/main/java/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/robot/subsystems/drivetrain/Drivetrain.java
@@ -25,6 +25,8 @@ public class Drivetrain extends Subsystem {
 
     private final Encoder leftEncoder = new Encoder(Ports.leftEncoderChannelA, Ports.leftEncoderChannelB);
     private final Encoder rightEncoder = new Encoder(Ports.rightEncoderChannelA, Ports.rightEncoderChannelB);
+    leftEncoder.setDistancePerPulse(Constants.PULSE_PER_DISTANCE);
+    rightEncoder.setDistancePerPulse(Constants.PULSE_PER_DISTANCE); 
 
     /**
      * Set the speed for both sides.
