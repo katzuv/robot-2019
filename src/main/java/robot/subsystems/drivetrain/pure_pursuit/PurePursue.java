e179fda63b2344cfdc003a989b62ef1066d355de
Orel Aboukasis <orel.david1502@gmail.com>
2018-10-25 19:08:55 +0300

    Create empty structure
    Created empty functions and methods to set what we will work on.

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/PurePursue.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/PurePursue.java
@@ -1,7 +1,6 @@
 package robot.subsystems.drivetrain.pure_pursuit;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.drive.Vector2d;
 import robot.Robot;
 import robot.subsystems.drivetrain.Drivetrain;
 
@@ -17,13 +16,12 @@ public class PurePursue extends Command {
     private Drivetrain drive;
 
     /**
-     *
      * @param path
      */
     public PurePursue(Path path) {
         drive = Robot.drivetrain;
         this.path = path;
-        robot = new Point(0,0);
+        robot = new Point(0, 0);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
@@ -56,39 +54,84 @@ public class PurePursue extends Command {
     protected void interrupted() {
     }
 
-    private void updatePoint(){
+    private void updatePoint() {
         //change in (change left encoder value + change in right encoder value)/2
-        double distance = ((drive.getLeftDistance() - lastLeftEncoder) + (drive.getRightDistance() - lastRightEncoder))/2;
-        robot.setX(robot.getX() + distance*Math.cos(drive.getAngle() * (Math.PI / 180.0)));
-        robot.setY(robot.getY() + distance*Math.sin(drive.getAngle() * (Math.PI / 180.0)));
+        double distance = ((drive.getLeftDistance() - lastLeftEncoder) + (drive.getRightDistance() - lastRightEncoder)) / 2;
+        robot.setX(robot.getX() + distance * Math.cos(drive.getAngle() * (Math.PI / 180.0)));
+        robot.setY(robot.getY() + distance * Math.sin(drive.getAngle() * (Math.PI / 180.0)));
 
         lastLeftEncoder = drive.getLeftDistance();
         lastRightEncoder = drive.getRightDistance();
     }
 
+    //Only Part of the function! doesn't run function through all of the path
     //https://stackoverflow.com/questions/1073336/circle-line-segment-collision-detection-algorithm/1084899#1084899
-    private Point findNearPath(Point ref, double lookahead, PathPoint point1, PathPoint point2){
+
+    /**
+     * @param ref
+     * @param lookahead
+     * @param point1
+     * @param point2
+     * @return
+     */
+    private Point findLookaheadInSegment(Point ref, double lookahead, PathPoint point1, PathPoint point2) {
         Vector p = new Vector(point2, point1);
         Vector f = new Vector(point1, ref);
 
         //p*p + 2*f*p + f*f - r*r = 0
         double a = p.dot(p);
-        double b = 2*f.dot(p);
+        double b = 2 * f.dot(p);
         double c = f.dot(f) - lookahead * lookahead;
-        double discriminant = b*b - 4*a*c;
+        double discriminant = b * b - 4 * a * c;
 
-        if(discriminant < 0)
+        if (discriminant < 0)
             return null; //means that the circle doesnt reach the line
-        else{
+        else {
             discriminant = Math.sqrt(discriminant);
-            double opt1 = (-b - discriminant)/(2*a);
-            double opt2 = (-b + discriminant)/(2*a);
-            if (opt1 >= 0 && opt1 <= 1){
+            double opt1 = (-b - discriminant) / (2 * a);
+            double opt2 = (-b + discriminant) / (2 * a);
+            if (opt1 >= 0 && opt1 <= 1) {
                 return p.multiply(opt1).add(point1);
             }
-            if(opt2 >=0 && opt2 <= 1)
+            if (opt2 >= 0 && opt2 <= 1)
                 return p.multiply(opt2).add(point1);
         }
         return null; //means that the segment is entirely inside the line.
     }
+
+    /**
+     * @return
+     */
+    private Point findLookaheadInPath() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    private Point closestPoint() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    private double curvatureCalculate() {
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public double getRightSpeed() {
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public double getLeftSpeed() {
+        return 0;
+    }
+
 }
