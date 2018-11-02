6d6f65d01c2991f61531743dc8ca236971f36c2f
Paulo Khayat <paulokhayat@gmail.com>
2018-11-02 12:29:33 +0200

    Remove all irreverent code
    copied them to their respective branches and removed them from here to prevent confusion

--- src/main/java/robot/MainTest.java
+++ src/main/java/robot/MainTest.java
@@ -8,17 +8,6 @@ public class MainTest {
 
 
     public static void main(String[] args) {
-        Path path = new Path();
-        path.append(new Waypoint(0, 0));
-        path.append(new Waypoint(5, -1));
-        path.append(new Waypoint (7, 1));
-        System.out.println(path);
-        path = path.generateFillPoint();
-        System.out.println(path);
-        Path newPath = path.generateSmoothing(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE);
-        System.out.println("path2");
-        System.out.println(path);
-
 
     }
 }
