85b0c25f1887917eb7d173da3e891a2c359077d0
lior <lior5858@gmail.com>
2018-11-06 17:52:21 +0200

    Adjust code

--- src/main/java/robot/MainTest.java
+++ src/main/java/robot/MainTest.java
@@ -9,9 +9,9 @@ public class MainTest {
 
     public static void main(String[] args) {
         Path path = new Path();
-        path.append(new Waypoint(0, 0));
-        path.append(new Waypoint(0.9, 0));
-        path.append(new Waypoint (1.3, -0.5));
+        path.appendWaypoint(new Waypoint(0, 0));
+        path.appendWaypoint(new Waypoint(0.9, 0));
+        path.appendWaypoint(new Waypoint (1.3, -0.5));
         System.out.println(path);
         path = path.generateFillPoint();
         System.out.println(path);
