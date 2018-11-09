fd24562898739b7370185230da550da8722977e2
Paulo Khayat <paulokhayat@gmail.com>
2018-11-09 12:58:51 +0200

    Create IsReversed and getSpeed

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -3,6 +3,9 @@ package robot.subsystems.drivetrain;
 public class Constants {
     static final double PULSE_PER_DISTANCE = 0.003451;
     public static final double SPACING_BETWEEN_WAYPOINTS = 0.1524; //meters
+    public static final boolean LEFT_REVERSED = false;
+    public static final boolean RIGHT_REVERSED = false;
+
 
 
 }
