dba6e348817a4f213d5b004c6b726f52c009d2a2
Lior <lior5858@gmail.com>
2018-11-09 11:15:00 +0200

    Adjusted maintest

--- src/main/java/robot/MainTest.java
+++ src/main/java/robot/MainTest.java
@@ -12,6 +12,7 @@ public class MainTest {
         path.appendWaypoint(new Waypoint(0, 0));
         path.appendWaypoint(new Waypoint(0.9, 0));
         path.appendWaypoint(new Waypoint (1.3, -0.5));
+        path.appendWaypoint(new Waypoint(5, 3));
         System.out.println(path);
         path = path.generateFillPoint();
         System.out.println(path);
