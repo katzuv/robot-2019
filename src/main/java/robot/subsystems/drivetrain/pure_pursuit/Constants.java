805644eb393e12e639d9cc799694f79075e8977f
Lior <lior5858@gmail.com>
2018-11-09 11:33:36 +0200

    Add Trackwidth to constants
    temporary value

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
     public final static double WEIGHT_DATA = 1-WEIGHT_SMOOTH;
     public final static double TOLERANCE = 0.001;
     public final static double LOOKAHEAD_DISTANCE = 0.3;
+    public final static double TRACK_WIDTH = 3;
 
 }
