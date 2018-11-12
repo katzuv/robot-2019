65b6f30e43728b2d095254a08a85655f6472f3d7
Paulo Khayat <paulokhayat@gmail.com>
2018-11-12 10:25:25 +0200

    Organize constants

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
@@ -1,13 +1,19 @@
 package robot.subsystems.drivetrain.pure_pursuit;
 
 public class Constants {
+
+    public final static double LOOKAHEAD_DISTANCE = 0.3; //in meters
+    public final static double TRACK_WIDTH = 3;
+
+    //SMOOTHING CONSTANTS (pure numbers)
     public final static double WEIGHT_SMOOTH = 0.85;
     public final static double WEIGHT_DATA = 1-WEIGHT_SMOOTH;
     public final static double TOLERANCE = 0.001;
-    public final static double LOOKAHEAD_DISTANCE = 0.3;
-    public final static double TRACK_WIDTH = 3;
+
+    //DRIVING CONSTANTS (pure numbers)
     public final static double Kv = 0;
     public final static double Ka = 0;
     public final static double Kp = 0;
-    public final static double STOP_SPEED_THRESH = 0.1;
+
+    public final static double STOP_SPEED_THRESH = 0.1; //the speed the robot could stop at the end of the path.
 }
