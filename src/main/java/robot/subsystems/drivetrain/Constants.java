054373ad92ea9f92ba747642841af415bbbfadc7
Paulo Khayat <paulokhayat@gmail.com>
2018-11-22 18:30:27 +0200

    Add limitRate
    Placed a Max_rate constant in the drive constants.

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     static final boolean LEFT_REVERSED = false;
     static final boolean RIGHT_REVERSED = false;
     static final double PULSE_PER_DISTANCE = 0.003451;
+    public static final double MAX_RATE = 0.3; //the maximum voltage acceleration (rate of change of the motor voltage per second)
+
 }
