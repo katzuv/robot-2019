dd3890418813dd1ae63b39a42f5cadd3855fb985
lior <lior5858@gmail.com>
2018-11-23 12:40:20 +0200

    Fix ports

--- src/main/java/robot/subsystems/drivetrain/Ports.java
+++ src/main/java/robot/subsystems/drivetrain/Ports.java
@@ -1,13 +1,13 @@
 package robot.subsystems.drivetrain;
 
 public class Ports {
-    final static int leftForwardMotor = 69;
-    final static int leftBackMotor = 69;
-    final static int rightForwardMotor = 69;
-    final static int rightBackMotor = 69;
-    final static int rightEncoderChannelA = 0;
-    final static int rightEncoderChannelB = 1;
-    final static int leftEncoderChannelA = 2;
-    final static int leftEncoderChannelB = 3;
+    final static int leftForwardMotor = 7;
+    final static int leftBackMotor = 6;
+    final static int rightForwardMotor = 8;
+    final static int rightBackMotor = 9;
+    final static int rightEncoderChannelA = 2;
+    final static int rightEncoderChannelB = 3;
+    final static int leftEncoderChannelA = 0;
+    final static int leftEncoderChannelB = 1;
 
 }
