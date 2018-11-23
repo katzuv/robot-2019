95b31bca928cf79e6305f2c0097ca147d9e58457
lior <lior5858@gmail.com>
2018-11-23 13:09:28 +0200

    Fix distance per pulse constant

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -1,7 +1,7 @@
 package robot.subsystems.drivetrain;
 
 public class Constants {
-    static final boolean LEFT_REVERSED = false;
+    static final boolean LEFT_REVERSED = true;
     static final boolean RIGHT_REVERSED = false;
-    static final double PULSE_PER_DISTANCE = 0.003451;
+    static final double PULSE_PER_DISTANCE = 0.00284051307;
 }
