21c24d441816122b988284617083b7b70d47b1ea
Paulo Khayat <paulokhayat@gmail.com>
2018-12-06 18:13:23 +0200

    Add generateAll in Maintest for testing

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/MainTest.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/MainTest.java
@@ -9,7 +9,11 @@ public class MainTest {
         path.appendWaypoint(new Waypoint(0.9, 0));
         path.appendWaypoint(new Waypoint(1.3, -0.5));
         path.appendWaypoint(new Waypoint(5, 3));
-
+        System.out.println("BASIC PATH:");
+        System.out.println(path);
         path.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
+        System.out.println("AFTER ALL GENERATION:");
+        System.out.println(path);
+
     }
 }
