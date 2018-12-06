29b6089b7e4c3b3dfce0d2f6e1680af3c6222b5d
Paulo Khayat <paulokhayat@gmail.com>
2018-12-06 17:59:17 +0200

    Match MainTest to new generateVelocity()

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/MainTest.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/MainTest.java
@@ -9,14 +9,7 @@ public class MainTest {
         path.appendWaypoint(new Waypoint(0.9, 0));
         path.appendWaypoint(new Waypoint(1.3, -0.5));
         path.appendWaypoint(new Waypoint(5, 3));
-        System.out.println(path);
-        path = path.generateFillPoint();
-        System.out.println(path);
-        Path newPath = path.generateSmoothing(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE);
-        System.out.println("path2");
-        newPath.generateCurvature();
-        newPath.generateVelocity(Constants.MAX_ACCEL);
-        System.out.println(newPath);
 
+        path.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
     }
 }
