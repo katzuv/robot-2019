c434ba8c5ed51f372f327b0e036ed167afdcb662
Paulo Khayat <paulokhayat@gmail.com>
2018-12-07 10:46:24 +0200

    Improve mainTest

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/MainTest.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/MainTest.java
@@ -8,12 +8,10 @@ class MainTest {
         path.appendWaypoint(new Waypoint(0, 0));
         path.appendWaypoint(new Waypoint(0, 1));
         path.appendWaypoint(new Waypoint(1, 1));
-        path = path.generateFillPoint();
-        path = path.generateSmoothing(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE);
-        path.generateCurvature();
-        path.generateDistance();
-        path.generateVelocity(Constants.MAX_ACCEL);
-        path.getWaypoint(0).setSpeed(path.getWaypoint(1).getSpeed()/2);
+        System.out.println("BASIC PATH:");
+        System.out.println(path);
+        path.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
+        System.out.println("AFTER ALL GENERATION:");
         System.out.println(path);
 
         System.out.println(Math.tan(Math.toRadians(90)));
