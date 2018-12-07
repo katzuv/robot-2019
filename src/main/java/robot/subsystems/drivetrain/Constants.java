820ea010b6a6be0ea26edc141cf7b5cae9e4749a
Paulo Khayat <paulokhayat@gmail.com>
2018-12-07 10:30:54 +0200

    Update Constants files, add necessary constants

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -1,8 +1,9 @@
 package robot.subsystems.drivetrain;
 
-class Constants {
+public class Constants {
     static final boolean LEFT_REVERSED = false;
     static final boolean RIGHT_REVERSED = true;
+    public static final double MAX_RATE = 0.3;
     static final double DISTANCE_PER_PULSE = (0.2032 * Math.PI) / 231;//diameter of the wheel is 0.2032 meters (8 inches), the encoder sends 226 pulses every 360 degree turn
 
 }
--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
@@ -4,21 +4,30 @@ public class Constants {
     //DRIVING CONSTANTS
     public final static double LOOKAHEAD_DISTANCE = 0.3; //in meters
     public static final double MAX_VELOCITY = 3.762;
-    final static double TRACK_WIDTH = 0.6; //width of the robot
+
 
     //PATH GENERATION CONSTANTS
     public static final double SPACING_BETWEEN_WAYPOINTS = 0.1524; //meters
     public static final double MAX_ACCEL = 0.3;
+
 
     //SMOOTHING CONSTANTS (pure numbers)
     public final static double WEIGHT_SMOOTH = 0.85;
     public final static double WEIGHT_DATA = 1 - WEIGHT_SMOOTH;
     public final static double TOLERANCE = 0.001;
 
+    //VELOCITY CONSTANTS
+    public static final double MAX_PATH_VELOCITY = 1.5;
+    static final double K_CURVE = 2; //number from 1 to 5
+
     //DRIVING CONSTANTS (pure numbers)
     public final static double kV = 0.75;
     public final static double kA = 0.000;
     public final static double kP = 0;
 
-    final static double STOP_SPEED_THRESH = 0.1; //the speed the robot could stop at the end of the path.
+    public final static double STOP_SPEED_THRESH = 0.1; //the speed the robot could stop at the end of the path.
+
+    public final static double CYCLE_TIME = 0.02; //cycle time of the roborio
+
+    public final static double ROBOT_WIDTH = 0.6; //width of the robot
 }
