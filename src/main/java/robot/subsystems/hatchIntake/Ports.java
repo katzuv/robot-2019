6b3a0d691c8bd8c4d063c6a444153108cfd81aa4
orel-david <orel.david1502@gmail.com>
2019-01-20 22:14:57 +0200

    Create ground intake subsystem

---
+++ src/main/java/robot/subsystems/hatchIntake/Ports.java
@@ -0,0 +1,7 @@
+package robot.subsystems.hatchIntake;
+
+public class Ports {
+    public final static int groundReverse = 0;
+    public final static int groundForward = 0;
+    public final static int prox = 0;
+}
