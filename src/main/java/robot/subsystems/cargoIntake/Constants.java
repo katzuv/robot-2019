397023e15e34c4d24862e4babdc7c9fd4d963924
lior <lior5858@gmail.com>
2019-01-21 18:13:43 +0200

    Added constants for the pidf config
    used by the wrist

--- src/main/java/robot/subsystems/cargoIntake/Constants.java
+++ src/main/java/robot/subsystems/cargoIntake/Constants.java
@@ -1,4 +1,9 @@
 package robot.subsystems.cargoIntake;
 
 public class Constants {
+    public final static double kP = 0;
+    public final static double kI = 0;
+    public final static double kD = 0;
+    public final static double kF = 0;
+    public final static int TimeOutMS = 0;
 }
