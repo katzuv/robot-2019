a5a0c1b9a293c87188d35ea44e78c5c9ee2ec76f
lior <lior5858@gmail.com>
2019-01-22 17:59:02 +0200

    Add constants for the wrist control

--- src/main/java/robot/subsystems/cargoIntake/Constants.java
+++ src/main/java/robot/subsystems/cargoIntake/Constants.java
@@ -6,4 +6,7 @@ public class Constants {
     public final static double kD = 0;
     public final static double kF = 0;
     public final static int TimeOutMS = 0;
+    public final static int  kSlotIdx = 0;
+    public final static int kPIDLoopIdx = 0;
+    public final static double WristRadius = 0;
 }
