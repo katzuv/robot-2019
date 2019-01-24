b6478d875d657aafffad97acfe77e9e2d45ab831
Dan Katzuv <31829093+katzuv@users.noreply.github.com>
2019-01-24 17:16:21 +0200

    Improve constants name
    Co-Authored-By: LiorBarkai58 <42713106+LiorBarkai58@users.noreply.github.com>

--- src/main/java/robot/subsystems/cargoIntake/Constants.java
+++ src/main/java/robot/subsystems/cargoIntake/Constants.java
@@ -5,7 +5,7 @@ class Constants {
     public final static double kI = 0;
     public final static double kD = 0;
     public final static double kF = 0;
-    public final static int TimeOutMS = 0;
+    public final static int TALON_TIME_OUT = 0;
     public final static int  kSlotIdx = 0;
     public final static int kPIDLoopIdx = 0;
     public final static double WristRadius = 0;
