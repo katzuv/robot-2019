32cd79254e9ec8bfb7167b65ecec174243835608
Paulo Khayat <paulokhayat@gmail.com>
2019-01-24 22:27:13 +0200

    Write port variables

--- src/main/java/robot/subsystems/elevator/Ports.java
+++ src/main/java/robot/subsystems/elevator/Ports.java
@@ -1,4 +1,9 @@
 package robot.subsystems.elevator;
 
 public class Ports {
+    final static int talonMotor = -1;
+    final static int victorMotor = -1;
+    final static int encoderChannelA = -1;
+    final static int encoderChannelB = -1;
+
 }
