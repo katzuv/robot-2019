713034f2c7e34fbecd09bf011cad20ccd9d3687d
Paulo Khayat <paulokhayat@gmail.com>
2019-01-25 10:00:36 +0200

    Add Talon constants

--- src/main/java/robot/subsystems/elevator/Constants.java
+++ src/main/java/robot/subsystems/elevator/Constants.java
@@ -1,4 +1,8 @@
 package robot.subsystems.elevator;
 
 public class Constants {
+
+    public static final int TALON_TIMEOUT_MS = 0;
+    public static final double DISTANCE_PER_PULSE = 0.0;
+    public static int[] LIFT_PIDF = {0, 0, 0 ,0};
 }
