adfecfe7768c62232a8e1514457f865304043838
Paulo Khayat <paulokhayat@gmail.com>
2019-01-26 17:14:55 +0200

    Narrow scope of constants to package-private

--- src/main/java/robot/subsystems/elevator/Constants.java
+++ src/main/java/robot/subsystems/elevator/Constants.java
@@ -2,12 +2,13 @@ package robot.subsystems.elevator;
 
 public class Constants {
 
-    public static final int TALON_TIMEOUT_MS = 0;
-    public static final double DISTANCE_PER_PULSE = 0.0;
-    public static final double[] LIFT_BOTTOM_DOWN_PIDF = {0,0,0,0};
-    public static final double[] LIFT_BOTTOM_UP_PIDF = {0,0,0,0};
-    public static final double[] LIFT_TOP_DOWN_PIDF = {0,0,0,0};
-    public static final double[] LIFT_TOP_UP_PIDF = {0,0,0,0};
+    static final double NOMINAL_OUT_FWD = 0;
+    static final int TALON_TIMEOUT_MS = 0;
+    static final double DISTANCE_PER_PULSE = 0.0;
+    static final double[] LIFT_BOTTOM_DOWN_PIDF = {0, 0, 0, 0};
+    static final double[] LIFT_BOTTOM_UP_PIDF = {0, 0, 0, 0};
+    static final double[] LIFT_TOP_DOWN_PIDF = {0, 0, 0, 0};
+    static final double[] LIFT_TOP_UP_PIDF = {0, 0, 0, 0};
     static final boolean TALON_REVERSE = false;
     static final boolean ENCODER_REVERSED = false;
     static final boolean TOP_HALL_REVERSED = false;
