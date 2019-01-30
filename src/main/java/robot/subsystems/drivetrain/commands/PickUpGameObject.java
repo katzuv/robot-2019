068249fd472786dca2c1a9d6bd8a36f5c57cc476
lior <lior5858@gmail.com>
2019-01-30 18:29:52 +0200

    Create PickUpGameObject Command
    Command to drive towards a game object

---
+++ src/main/java/robot/subsystems/drivetrain/commands/PickUpGameObject.java
@@ -0,0 +1,34 @@
+package robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import robot.subsystems.drivetrain.pure_pursuit.Constants;
+import robot.subsystems.drivetrain.pure_pursuit.Path;
+import robot.subsystems.drivetrain.pure_pursuit.PurePursue;
+import robot.subsystems.drivetrain.pure_pursuit.Waypoint;
+
+import static robot.Robot.navx;
+
+/**
+ *
+ */
+public class PickUpGamePiece extends CommandGroup {
+
+    public PickUpGamePiece() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
