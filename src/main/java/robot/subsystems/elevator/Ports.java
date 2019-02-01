9aa2a015b7571c75802f440e0317deb5a84873c5
Paulo Khayat <paulokhayat@gmail.com>
2019-02-01 15:15:23 +0200

    Remove useless encoder declarations

--- src/main/java/robot/subsystems/elevator/Ports.java
+++ src/main/java/robot/subsystems/elevator/Ports.java
@@ -3,7 +3,5 @@ package robot.subsystems.elevator;
 class Ports {
     final static int talonMotor = -1;
     final static int victorMotor = -1;
-    final static int encoderChannelA = -1;
-    final static int encoderChannelB = -1;
 
 }
