9381bda60f32809302b80d938c117948631f103f
lior <lior5858@gmail.com>
2019-02-02 21:13:46 +0200

    Change HatchOrCargo to a conditional command

--- src/main/java/robot/subsystems/drivetrain/commands/HatchOrCargo.java
+++ src/main/java/robot/subsystems/drivetrain/commands/HatchOrCargo.java
@@ -1,29 +1,32 @@
 package robot.subsystems.drivetrain.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.ConditionalCommand;
 
 /**
  *this command is called when the pickupgamepiece commandgroup is called, when the robot wants to pick up the nearest game piece
  * this command is called after the robot is next to the piece and decides weather to activate the hatch intake or the cargo intake
  */
-public class HatchOrCargo extends Command {
+public class HatchOrCargo extends ConditionalCommand {
 
-    public HatchOrCargo() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+
+    public HatchOrCargo(HatchIntake onTrue, CargoIntake onFalse) {
+        super(onTrue, onFalse);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        boolean pickHatch = true;//will later be smartdashboard input from image detection
-        if (pickHatch)
-            new HatchIntake().start();
-        else
-            new CargoIntake().start();
+    
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    }
+
+    @Override
+    protected boolean condition() {
+        boolean pickHatch = true;//will later be smartdashboard input from image detection
+        return pickHatch;
     }
 
     // Make this return true when this Command no longer needs to run execute()
