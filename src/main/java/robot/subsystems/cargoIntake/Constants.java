b3d53f19da88711a8f7acc115c730d9ecfeea2b7
lior <lior5858@gmail.com>
2019-02-02 21:38:56 +0200

    Add java doc to constant

--- src/main/java/robot/subsystems/cargoIntake/Constants.java
+++ src/main/java/robot/subsystems/cargoIntake/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public final static double CARGO_IN_VOLTAGE = 2.5;//felt cute might delete later
     public final static double GRIPPER_SPEED = 0.75;
     public final static double INTAKE_ANGLE = 0;
-    public final static double FOLDED_ANGLE = 165;//felt cute might delete later
+    public final static double FOLDED_ANGLE = 165;//folded angle represents the angle in which the wrist is folded back inside the robot (number felt cute might delete later)
     public final static boolean WRIST_LIMIT_REVESED = true;//might need to be changed
     public final static double INITIAL_ANGLE = 0;//initial angle of the wrist
 }
