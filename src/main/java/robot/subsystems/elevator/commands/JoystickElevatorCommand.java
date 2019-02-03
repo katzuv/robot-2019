5b67a9fc2ef41c949d8e6a3059495dcd83aba680
Paulo Khayat <paulokhayat@gmail.com>
2019-02-03 20:40:07 +0200

    More name refactoring in Joystick command

--- src/main/java/robot/subsystems/elevator/commands/JoystickElevatorCommand.java
+++ src/main/java/robot/subsystems/elevator/commands/JoystickElevatorCommand.java
@@ -23,17 +23,17 @@ public class JoystickElevatorCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        double y = -Robot.m_oi.xbox.getY(); // invert the input to make up positive and down negative
+        double yAxis = -Robot.m_oi.xbox.getY(); // invert the input to make up positive and down negative
         if (!Robot.m_oi.xbox.getRawButton(10))
             return;
         // MAPPING (|dead-band to 1| -> |0 to 1|)
-        y -= y > 0 ? OI.XBOX_JOYSTICK_DEAD_BAND : -OI.XBOX_JOYSTICK_DEAD_BAND;
-        y *= 1 / (1 - OI.XBOX_JOYSTICK_DEAD_BAND);
+        yAxis -= yAxis > 0 ? OI.XBOX_JOYSTICK_DEAD_BAND : -OI.XBOX_JOYSTICK_DEAD_BAND;
+        yAxis *= 1 / (1 - OI.XBOX_JOYSTICK_DEAD_BAND);
         double change;
-        if (y > 0)
-            change = y * OI.UP_SPEED_RATE;
+        if (yAxis > 0)
+            change = yAxis * OI.UP_SPEED_RATE;
         else
-            change = y * OI.DOWN_SPEED_RATE;
+            change = yAxis * OI.DOWN_SPEED_RATE;
         elevator.setHeight(elevator.getHeight() + change);
     }
 
