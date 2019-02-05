fcf6f4cc2b3b1e76c0a3867447b1d34e3fb69ea5
lior <lior5858@gmail.com>
2019-02-05 15:58:35 +0200

    Update ports for the driving robot

--- src/main/java/robot/subsystems/drivetrain/Ports.java
+++ src/main/java/robot/subsystems/drivetrain/Ports.java
@@ -1,10 +1,10 @@
 package robot.subsystems.drivetrain;
 
 public class Ports {
-    final static int leftForwardMotor = 8;
-    final static int leftBackMotor = 9;
-    final static int rightForwardMotor = 7;
-    final static int rightBackMotor = 6;
+    final static int leftForwardMotor = 3;
+    final static int leftBackMotor = 2;
+    final static int rightForwardMotor = 0;
+    final static int rightBackMotor = 1;
     final static int rightEncoderChannelA = 2;
     final static int rightEncoderChannelB = 3;
     final static int leftEncoderChannelA = 0;
