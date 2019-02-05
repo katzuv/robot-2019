78c5b519a1c65222ffcd7f59d81d6463a249ac2c
Paulo Khayat <paulokhayat@gmail.com>
2019-02-05 21:04:09 +0200

    Implement enum usage to the gripper

--- src/main/java/robot/subsystems/hatch_intake/commands/Gripper.java
+++ src/main/java/robot/subsystems/hatch_intake/commands/Gripper.java
@@ -2,32 +2,64 @@ package robot.subsystems.hatch_intake.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import robot.Robot;
+import robot.subsystems.hatch_intake.HatchIntake;
 
 public class Gripper extends Command {
     private boolean open;
 
+    private boolean open;//indicates whether the flower is open or not
+    private HatchIntake hatchIntake = new HatchIntake();
+    private opMode current;//enum variable that indicates the current mode of the gripper
+
+
+    /**
+     * empty constructor, sets the wanted state to toggle meaning whenever the command is called it will toggle the current state
+     * instead of going to the wanted state
+     */
+    public Gripper() {
+        requires(Robot.hatchIntake);
+        current = opMode.TOGGLE;
+
+    }
+
+    /**
+     * constructor that sets the wanted state
+     *
+     * @param open if true changes the wanted state to open and otherwise sets the wanted state to cloes
+     */
     public Gripper(boolean open) {
         requires(Robot.hatchIntake);
-        this.open = open;
+        if (open)
+            current = opMode.OPEN;
+        else
+            current = opMode.CLOSE;
     }
 
     @Override
     public void initialize() {
-
-    }
-
-    @Override
-    public void execute() {
-        if (open) {
-            Robot.hatchIntake.gripperOpen();//grab hatch
-        } else {
-            Robot.hatchIntake.gripperClose();//release hatch
+        switch (current) {
+            case TOGGLE: // Change to the second state
+                hatchIntake.setGripper();
+                break;
+            case OPEN: // Open the gripper if closed and not do anything otherwise
+                if (!hatchIntake.isGripperOpen())
+                    hatchIntake.setGripper();
+                break;
+            case CLOSE:// Close the gripper if opened and not do anything otherwise
+                if (hatchIntake.isGripperOpen())
+                    hatchIntake.setGripper();
+                break;
         }
     }
 
     @Override
+    public void execute() {
+
+    }
+
+    @Override
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     @Override
@@ -38,5 +70,14 @@ public class Gripper extends Command {
     // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+    }
+
+    /**
+     * enum to indicate the state of the gripper
+     */
+    public enum opMode {
+        TOGGLE,
+        OPEN,
+        CLOSE
     }
 }
