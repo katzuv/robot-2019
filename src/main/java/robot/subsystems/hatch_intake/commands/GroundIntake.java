139f684b59a0de73d295125ab4550d4820886abb
Paulo Khayat <paulokhayat@gmail.com>
2019-02-07 09:29:40 +0200

    Add comment to GroundIntake command group
    The GroundIntake command group isn't viable as of now.

--- src/main/java/robot/subsystems/hatch_intake/commands/GroundIntake.java
+++ src/main/java/robot/subsystems/hatch_intake/commands/GroundIntake.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class GroundIntake extends CommandGroup {
 
-    public GroundIntake() {
-            addSequential(new HatchTransportation());//lift hatch if inside
+    public GroundIntake() {//TODO: this command isn't right. we need to fix it in the future. theres no testing whether the hatch is inside, and no safety features.
+            addSequential(new HatchTransportation(false));//lift hatch if inside
             addSequential(new Gripper(false));//put it on the flower
     }
 }
