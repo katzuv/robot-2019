3f63f0941d57d5d9fd0231d3c9920a4157795550
orel-david <orel.david1502@gmail.com>
2019-02-08 14:43:19 +0200

    Move the generation from vision to it's own function

--- src/main/java/robot/subsystems/drivetrain/commands/GamePiecePickup.java
+++ src/main/java/robot/subsystems/drivetrain/commands/GamePiecePickup.java
@@ -5,13 +5,8 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import robot.subsystems.drivetrain.pure_pursuit.Constants;
-import robot.subsystems.drivetrain.pure_pursuit.Path;
-import robot.subsystems.drivetrain.pure_pursuit.PurePursue;
-import robot.subsystems.drivetrain.pure_pursuit.Waypoint;
-
-import static robot.Robot.drivetrain;
-import static robot.Robot.navx;
+import robot.Robot;
+import robot.subsystems.drivetrain.pure_pursuit.*;
 
 /**
  *
@@ -32,15 +27,11 @@ public class GamePiecePickup extends Command {
         NetworkTable table = inst.getTable("vision");
         targetAngleEntry = table.getEntry("angle");
         targetDistanceEntry = table.getEntry("distance");
-        double targetDistance = targetDistanceEntry.getDouble(0)/100;
-        double targetAngle = targetAngleEntry.getDouble(0);
-        Waypoint target = new Waypoint(Math.sin(Math.toRadians(targetAngle)) * targetDistance +0.15, Math.cos(Math.toRadians(targetAngle)) * targetDistance   );
-        Waypoint middleWP = new Waypoint(0, target.getY()-target.getY()/2);
-        Path path1 = new Path(new Waypoint[]{new Waypoint(0,0), middleWP ,  target});
-        System.out.println(path1);
+
+        Path path1 = generateFromVision(targetAngleEntry.getDouble(0), targetDistanceEntry.getDouble(0));
         path1.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
-        SmartDashboard.putNumber("target distance", targetDistance);
-        SmartDashboard.putNumber("target angle ", targetAngle);
+        SmartDashboard.putNumber("target distance", targetDistanceEntry.getDouble(0) / 100);
+        SmartDashboard.putNumber("target angle ", targetAngleEntry.getDouble(0));
         PurePursue pursue = new PurePursue(path1, Constants.LOOKAHEAD_DISTANCE, Constants.kP, Constants.kA, Constants.kV, true, false);
         pursue.start();
         System.out.println(path1);
@@ -62,5 +53,27 @@ public class GamePiecePickup extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    }
+
+    private Path generateFromVision(double angle, double distance) {
+        double targetDistance = distance / 100;
+        double targetAngle = angle;
+        Waypoint target = target(targetAngle, targetDistance);
+        Waypoint middleWP = middleWP(target);
+        Path path1 = new Path(new Waypoint[]{new Waypoint(0, 0), middleWP, target});
+        System.out.println(path1);
+        return path1;
+    }
+
+    private Waypoint middleWP(Waypoint target) {
+        Waypoint middleWP = new Waypoint(0, target.getY() - target.getY() / 2);
+        return middleWP;
+    }
+
+    private Waypoint target(double angle, double distance) {
+        double targetDistance = distance / 100;
+        double targetAngle = angle;
+        Waypoint target = new Waypoint(Math.sin(Math.toRadians(targetAngle)) * targetDistance + 0.15, Math.cos(Math.toRadians(targetAngle)) * targetDistance);
+        return target;
     }
 }
