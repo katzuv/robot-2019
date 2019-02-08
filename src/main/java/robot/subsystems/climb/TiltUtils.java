90c6e7b269ee223640ef8c4855510e9b67cb8f0a
Paulo Khayat <paulokhayat@gmail.com>
2019-02-08 22:52:55 +0200

    Write second shell constructor for getRotatedPoint

--- src/main/java/robot/subsystems/climb/TiltUtils.java
+++ src/main/java/robot/subsystems/climb/TiltUtils.java
@@ -1,18 +1,26 @@
 package robot.subsystems.climb;
 
+import javafx.geometry.Point2D;
 import javafx.geometry.Point3D;
 
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
 public class TiltUtils {
-    public Point3D getRotatedPoint(Point3D p, double pitch, double roll, double distanceRight, double distanceForward){
+    public Point3D getRotatedPoint( double distanceRight, double distanceForward, double pitch, double roll){
         pitch = Math.toRadians(pitch);
         roll = Math.toRadians(roll);
 
         //Here we use euclidian rotation to rotate the point to its current position
         return new Point3D(distanceForward * cos(pitch) + distanceRight * sin(pitch) * sin(roll),
                 distanceRight * cos(roll),
-                distanceForward * sin(pitch) + distanceRight * cos(pitch));
+                distanceForward * sin(pitch) + distanceRight * cos(pitch)); //todo: im 90% percent sure i fricked something up when moving from right hand math to left hand programming.
+
+    }//TODO: assert length similarity
+
+    public Point3D getRotatedPoint(Point2D point, double pitch, double roll){
+        return getRotatedPoint(point.getX(), point.getY(), pitch, roll);
     }
 }
