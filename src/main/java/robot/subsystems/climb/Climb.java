27ed13251fcfc14be989e8dfe489e923b675d3fd
Paulo Khayat <paulokhayat@gmail.com>
2019-02-08 18:19:01 +0200

    Refactor up and down to forward and backwards

--- src/main/java/robot/subsystems/climb/Climb.java
+++ src/main/java/robot/subsystems/climb/Climb.java
@@ -18,74 +18,73 @@ public class Climb extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-    private TalonSRX talonUL = new TalonSRX(Ports.upLeftMotor); //TODO: change to forward left forwrard right
-    private TalonSRX talonUR = new TalonSRX(Ports.upRightMotor);
-    private TalonSRX talonDL = new TalonSRX(Ports.downLeftMotor);
-    private TalonSRX talonDR = new TalonSRX(Ports.downRightMotor);
+    private TalonSRX talonFL = new TalonSRX(Ports.forwardLeftMotor);
+    private TalonSRX talonFR = new TalonSRX(Ports.forwardRightMotor);
+    private TalonSRX talonBL = new TalonSRX(Ports.backLeftMotor);
+    private TalonSRX talonBR = new TalonSRX(Ports.backRightMotor);
 
     public Climb(){ //TODO: add four encoders to each of the motors just as in the elevator code.
-        talonUL.setInverted(Constants.UP_LEFT_TALON_REVERSE);
-        talonUR.setInverted(Constants.UP_RIGHT_TALON_REVERSE);
-        talonDL.setInverted(Constants.DOWN_LEFT_TALON_REVERSE);
-        talonDR.setInverted(Constants.DOWN_RIGHT_TALON_REVERSE);
+        talonFL.setInverted(Constants.FORWARD_LEFT_TALON_REVERSE);
+        talonFR.setInverted(Constants.FORWARD_RIGHT_TALON_REVERSE);
+        talonBL.setInverted(Constants.BACK_LEFT_TALON_REVERSE);
+        talonBR.setInverted(Constants.BACK_RIGHT_TALON_REVERSE);
 
         //what the motor does when not given voltage (Brake - decelerate the motor, Coast - not stop the motor)
-        talonUL.setNeutralMode(NeutralMode.Brake);
-        talonUR.setNeutralMode(NeutralMode.Brake);
-        talonDL.setNeutralMode(NeutralMode.Brake);
-        talonDR.setNeutralMode(NeutralMode.Brake);
+        talonFL.setNeutralMode(NeutralMode.Brake);
+        talonFR.setNeutralMode(NeutralMode.Brake);
+        talonBL.setNeutralMode(NeutralMode.Brake);
+        talonBR.setNeutralMode(NeutralMode.Brake);
 
         /* set closed loop gains in slot0 */
-        talonUL.config_kP(0, Constants.CLIMB_PIDF[0], Constants.TALON_TIMEOUT_MS);
-        talonUL.config_kI(0, Constants.CLIMB_PIDF[1], Constants.TALON_TIMEOUT_MS);
-        talonUL.config_kD(0, Constants.CLIMB_PIDF[2], Constants.TALON_TIMEOUT_MS);
-        talonUL.config_kF(0, Constants.CLIMB_PIDF[3], Constants.TALON_TIMEOUT_MS);
-
-        /* set closed loop gains in slot0 */
-        //I chose opposite arms because its mostly arbitrary and it might be more accurate, we want to have the monitoring on opposite sides
-        talonDR.config_kP(0, Constants.CLIMB_PIDF[0], Constants.TALON_TIMEOUT_MS);
-        talonDR.config_kI(0, Constants.CLIMB_PIDF[1], Constants.TALON_TIMEOUT_MS);
-        talonDR.config_kD(0, Constants.CLIMB_PIDF[2], Constants.TALON_TIMEOUT_MS);
-        talonDR.config_kF(0, Constants.CLIMB_PIDF[3], Constants.TALON_TIMEOUT_MS);
-
-        /* set closed loop gains in slot0 */
-        talonUR.config_kP(0, Constants.CLIMB_PIDF[0], Constants.TALON_TIMEOUT_MS);
-        talonUR.config_kI(0, Constants.CLIMB_PIDF[1], Constants.TALON_TIMEOUT_MS);
-        talonUR.config_kD(0, Constants.CLIMB_PIDF[2], Constants.TALON_TIMEOUT_MS);
-        talonUR.config_kF(0, Constants.CLIMB_PIDF[3], Constants.TALON_TIMEOUT_MS);
+        talonFL.config_kP(0, Constants.CLIMB_PIDF[0], Constants.TALON_TIMEOUT_MS);
+        talonFL.config_kI(0, Constants.CLIMB_PIDF[1], Constants.TALON_TIMEOUT_MS);
+        talonFL.config_kD(0, Constants.CLIMB_PIDF[2], Constants.TALON_TIMEOUT_MS);
+        talonFL.config_kF(0, Constants.CLIMB_PIDF[3], Constants.TALON_TIMEOUT_MS);
 
         /* set closed loop gains in slot0 */
         //I chose opposite arms because its mostly arbitrary and it might be more accurate, we want to have the monitoring on opposite sides
-        talonDL.config_kP(0, Constants.CLIMB_PIDF[0], Constants.TALON_TIMEOUT_MS);
-        talonDL.config_kI(0, Constants.CLIMB_PIDF[1], Constants.TALON_TIMEOUT_MS);
-        talonDL.config_kD(0, Constants.CLIMB_PIDF[2], Constants.TALON_TIMEOUT_MS);
-        talonDL.config_kF(0, Constants.CLIMB_PIDF[3], Constants.TALON_TIMEOUT_MS);
+        talonBR.config_kP(0, Constants.CLIMB_PIDF[0], Constants.TALON_TIMEOUT_MS);
+        talonBR.config_kI(0, Constants.CLIMB_PIDF[1], Constants.TALON_TIMEOUT_MS);
+        talonBR.config_kD(0, Constants.CLIMB_PIDF[2], Constants.TALON_TIMEOUT_MS);
+        talonBR.config_kF(0, Constants.CLIMB_PIDF[3], Constants.TALON_TIMEOUT_MS);
+
+        /* set closed loop gains in slot0 */
+        talonFR.config_kP(0, Constants.CLIMB_PIDF[0], Constants.TALON_TIMEOUT_MS);
+        talonFR.config_kI(0, Constants.CLIMB_PIDF[1], Constants.TALON_TIMEOUT_MS);
+        talonFR.config_kD(0, Constants.CLIMB_PIDF[2], Constants.TALON_TIMEOUT_MS);
+        talonFR.config_kF(0, Constants.CLIMB_PIDF[3], Constants.TALON_TIMEOUT_MS);
+
+        /* set closed loop gains in slot0 */
+        //I chose opposite arms because its mostly arbitrary and it might be more accurate, we want to have the monitoring on opposite sides
+        talonBL.config_kP(0, Constants.CLIMB_PIDF[0], Constants.TALON_TIMEOUT_MS);
+        talonBL.config_kI(0, Constants.CLIMB_PIDF[1], Constants.TALON_TIMEOUT_MS);
+        talonBL.config_kD(0, Constants.CLIMB_PIDF[2], Constants.TALON_TIMEOUT_MS);
+        talonBL.config_kF(0, Constants.CLIMB_PIDF[3], Constants.TALON_TIMEOUT_MS);
 
         //set the pairs to follow their adjacent arms
-        talonUR.follow(talonUL);
-        talonDL.follow(talonDR);
+        talonFR.follow(talonFL);
+        talonBL.follow(talonBR);
 
-        configMotorEncoder(talonUL, Constants.UP_LEFT_FORWARD_HALL_REVERSED, Constants.UP_LEFT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
-        configMotorEncoder(talonDR, Constants.DOWN_RIGHT_FORWARD_HALL_REVERSED, Constants.DOWN_RIGHT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
-        configMotorEncoder(talonUR, Constants.UP_RIGHT_FORWARD_HALL_REVERSED, Constants.UP_RIGHT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
-        configMotorEncoder(talonDL, Constants.DOWN_LEFT_FORWARD_HALL_REVERSED, Constants.DOWN_LEFT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
+        configMotorEncoder(talonFL, Constants.FORWARD_LEFT_FORWARD_HALL_REVERSED, Constants.FORWARD_LEFT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
+        configMotorEncoder(talonBR, Constants.BACK_RIGHT_FORWARD_HALL_REVERSED, Constants.BACK_RIGHT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
+        configMotorEncoder(talonFR, Constants.FORWARD_RIGHT_FORWARD_HALL_REVERSED, Constants.FORWARD_RIGHT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
+        configMotorEncoder(talonBL, Constants.BACK_LEFT_FORWARD_HALL_REVERSED, Constants.BACK_LEFT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
     }
 
     public void setTalonULHeight(double height){
-        talonUL.set(ControlMode.MotionMagic, metersToTicks(height));
+        talonFL.set(ControlMode.MotionMagic, metersToTicks(height));
     }
 
     public void setTalonURHeight(double height){
-        talonUR.set(ControlMode.MotionMagic, metersToTicks(height));
+        talonFR.set(ControlMode.MotionMagic, metersToTicks(height));
     }
     public void setTalonDLHeight(double height){
-        talonDL.set(ControlMode.MotionMagic, metersToTicks(height));
+        talonBL.set(ControlMode.MotionMagic, metersToTicks(height));
     }
     public void setTalonDRHeight(double height){
-        talonDR.set(ControlMode.MotionMagic, metersToTicks(height));
+        talonBR.set(ControlMode.MotionMagic, metersToTicks(height));
     }
 
-    /**
 
     private void configMotorEncoder(TalonSRX motorController, boolean forwardLSReversed, boolean backwardLSReversed, FeedbackDevice feedbackDevice){
         motorController.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, Constants.TALON_TIMEOUT_MS);
