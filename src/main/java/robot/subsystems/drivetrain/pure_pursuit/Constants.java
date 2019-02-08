b0ee7c03790af3fefe92ab834e32c5f582d9247c
lior <lior5858@gmail.com>
2019-02-08 13:51:35 +0200

    Change constants
    -Most major one is change the robot width to its actual number
    -play around with some constants for better precision
    -increase the maximum possible acceleration

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
@@ -3,7 +3,7 @@ package robot.subsystems.drivetrain.pure_pursuit;
 public class Constants {
     //DRIVING CONSTANTS
 
-    public final static double LOOKAHEAD_DISTANCE = 0.45; //in meters
+    public final static double LOOKAHEAD_DISTANCE = 0.65; //in meters
     public static final double MAX_VELOCITY = 3.762;
 
     //PATH GENERATION CONSTANTS
@@ -16,17 +16,17 @@ public class Constants {
 
     //VELOCITY CONSTANTS
     public static final double MAX_PATH_VELOCITY = 2;
-    public static final double MAX_ACCEL = 0.3;
+    public static final double MAX_ACCEL = 0.5;
     public static final double K_CURVE = 3; //number from 1 to 5
 
     //DRIVING CONSTANTS (pure numbers)
     public final static double kV = 1 / MAX_VELOCITY;
-    public final static double kA = 0.002;
+    public final static double kA = 0.004;
     public final static double kP = 0.01;
 
     public final static double STOP_SPEED_THRESH = 0.1; //the speed the robot could stop at the end of the path.
 
     public final static double CYCLE_TIME = 0.02; //cycle time of the roborio
 
-    public final static double ROBOT_WIDTH = 0.6; //width of the robot
+    public final static double ROBOT_WIDTH = 0.9; //width of the robot
 }
