9bf2ce6d37c72b3cf78c2c60305af75eac0ba205
Paulo Khayat <paulokhayat@gmail.com>
2019-02-08 22:01:04 +0200

    Write euclidian rotation on three axis'

--- src/main/java/robot/subsystems/climb/TiltUtils.java
+++ src/main/java/robot/subsystems/climb/TiltUtils.java
@@ -1,4 +1,18 @@
 package robot.subsystems.climb;
 
+import javafx.geometry.Point3D;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 public class TiltUtils {
+    public Point3D getRotatedPoint(Point3D p, double pitch, double roll, double distanceRight, double distanceForward){
+        pitch = Math.toRadians(pitch);
+        roll = Math.toRadians(roll);
+
+        //Here we use euclidian rotation to rotate the point to its current position
+        return new Point3D(distanceForward * cos(pitch) + distanceRight * sin(pitch) * sin(roll),
+                distanceRight * cos(roll),
+                distanceForward * sin(pitch) + distanceRight * cos(pitch));
+    }
 }
