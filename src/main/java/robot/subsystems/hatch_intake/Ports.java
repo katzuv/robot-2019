a4eb0f996361c2477dbc90397468bfea0faf9f60
lior <lior5858@gmail.com>
2019-02-12 20:23:45 +0200

    Add ports for one solenoid

--- src/main/java/robot/subsystems/hatch_intake/Ports.java
+++ src/main/java/robot/subsystems/hatch_intake/Ports.java
@@ -5,7 +5,7 @@ public class Ports {
     public final static int groundForward = 0;
     public final static int gripperReverse = 0;
     public final static int gripperForward = 0;
-    public final static int gripperPlateReverse = 0;
-    public final static int gripperPlateForward = 0;
+    public final static int gripperPlateReverse = 3;
+    public final static int gripperPlateForward = 4;
     public final static int proximitySensor = 0;
 }
