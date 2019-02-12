43877287b16d0ffabf0618b6a99608bc69b60441
lior <lior5858@gmail.com>
2019-02-12 22:00:10 +0200

    Update ports

--- src/main/java/robot/subsystems/hatch_intake/Ports.java
+++ src/main/java/robot/subsystems/hatch_intake/Ports.java
@@ -1,11 +1,9 @@
 package robot.subsystems.hatch_intake;
 
 public class Ports {
-    public final static int groundReverse = 0;
-    public final static int groundForward = 0;
-    public final static int gripperReverse = 0;
-    public final static int gripperForward = 0;
+    public final static int gripperReverse = 1;
+    public final static int gripperForward = 2;
     public final static int gripperPlateReverse = 3;
     public final static int gripperPlateForward = 4;
-    public final static int proximitySensor = 0;
+    public final static int proximitySensor = 7;
 }
