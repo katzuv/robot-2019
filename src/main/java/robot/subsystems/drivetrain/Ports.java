690396c4c7d366a7d0c08ef3cc9559c8f1ec95c9
Dan Katzuv <dan.dan.kafe@gmail.com>
2019-02-12 21:50:41 +0200

    Update motors CAN-Bus IDs

--- src/main/java/robot/subsystems/drivetrain/Ports.java
+++ src/main/java/robot/subsystems/drivetrain/Ports.java
@@ -1,10 +1,10 @@
 package robot.subsystems.drivetrain;
 
 public class Ports {
-    final static int leftMaster = -1;
-    final static int leftSlave1 = -1;
-    public static int leftSlave2 = -1;
-    final static int rightMaster = -1;
-    final static int rightSlave1 = -1;
-    public static int rightSlave2 = -1;
+    final static int leftMaster = 12;
+    final static int leftSlave1 = 4;
+    public static int leftSlave2 = 5;
+    final static int rightMaster = 6;
+    final static int rightSlave1 = 7;
+    public static int rightSlave2 = 8;
 }
