9a1ed6242432c89c5340c9d490438fab7b0b9471
Paulo Khayat <paulokhayat@gmail.com>
2019-02-12 13:48:09 +0200

    move motor setup into method

--- src/main/java/robot/subsystems/climb/Climb.java
+++ src/main/java/robot/subsystems/climb/Climb.java
@@ -26,47 +26,15 @@ public class Climb extends Subsystem {
     private TalonSRX talonBR = new TalonSRX(Ports.backRightMotor);
 
     public Climb() { //TODO: add four encoders to each of the motors just as in the elevator code.
-        talonFL.setInverted(Constants.FRONT_LEFT_TALON_REVERSE);
-        talonFR.setInverted(Constants.FRONT_RIGHT_TALON_REVERSE);
-        talonBL.setInverted(Constants.BACK_LEFT_TALON_REVERSE);
-        talonBR.setInverted(Constants.BACK_RIGHT_TALON_REVERSE);
+        configMotorMovement(talonFL, Constants.FRONT_LEFT_TALON_REVERSE, NeutralMode.Brake, Constants.CLIMB_PIDFE, Constants.TALON_TIMEOUT_MS);
+        configMotorMovement(talonFR, Constants.FRONT_RIGHT_TALON_REVERSE, NeutralMode.Brake, Constants.CLIMB_PIDFE, Constants.TALON_TIMEOUT_MS);
+        configMotorMovement(talonBL, Constants.BACK_LEFT_TALON_REVERSE, NeutralMode.Brake, Constants.CLIMB_PIDFE, Constants.TALON_TIMEOUT_MS);
+        configMotorMovement(talonBR, Constants.BACK_RIGHT_TALON_REVERSE, NeutralMode.Brake, Constants.CLIMB_PIDFE, Constants.TALON_TIMEOUT_MS);
 
-        //what the motor does when not given voltage (Brake - decelerate the motor, Coast - not stop the motor)
-        talonFL.setNeutralMode(NeutralMode.Brake);
-        talonFR.setNeutralMode(NeutralMode.Brake);
-        talonBL.setNeutralMode(NeutralMode.Brake);
-        talonBR.setNeutralMode(NeutralMode.Brake);
-
-        /* set closed loop gains in slot0 */
-        talonFL.config_kP(0, Constants.CLIMB_PIDFE[0], Constants.TALON_TIMEOUT_MS);
-        talonFL.config_kI(0, Constants.CLIMB_PIDFE[1], Constants.TALON_TIMEOUT_MS);
-        talonFL.config_kD(0, Constants.CLIMB_PIDFE[2], Constants.TALON_TIMEOUT_MS);
-        talonFL.config_kF(0, Constants.CLIMB_PIDFE[3], Constants.TALON_TIMEOUT_MS);
-
-        /* set closed loop gains in slot0 */
-        //I chose opposite arms because its mostly arbitrary and it might be more accurate, we want to have the monitoring on opposite sides
-        talonBR.config_kP(0, Constants.CLIMB_PIDFE[0], Constants.TALON_TIMEOUT_MS);
-        talonBR.config_kI(0, Constants.CLIMB_PIDFE[1], Constants.TALON_TIMEOUT_MS);
-        talonBR.config_kD(0, Constants.CLIMB_PIDFE[2], Constants.TALON_TIMEOUT_MS);
-        talonBR.config_kF(0, Constants.CLIMB_PIDFE[3], Constants.TALON_TIMEOUT_MS);
-
-        /* set closed loop gains in slot0 */
-        talonFR.config_kP(0, Constants.CLIMB_PIDFE[0], Constants.TALON_TIMEOUT_MS);
-        talonFR.config_kI(0, Constants.CLIMB_PIDFE[1], Constants.TALON_TIMEOUT_MS);
-        talonFR.config_kD(0, Constants.CLIMB_PIDFE[2], Constants.TALON_TIMEOUT_MS);
-        talonFR.config_kF(0, Constants.CLIMB_PIDFE[3], Constants.TALON_TIMEOUT_MS);
-
-        /* set closed loop gains in slot0 */
-        //I chose opposite arms because its mostly arbitrary and it might be more accurate, we want to have the monitoring on opposite sides
-        talonBL.config_kP(0, Constants.CLIMB_PIDFE[0], Constants.TALON_TIMEOUT_MS);
-        talonBL.config_kI(0, Constants.CLIMB_PIDFE[1], Constants.TALON_TIMEOUT_MS);
-        talonBL.config_kD(0, Constants.CLIMB_PIDFE[2], Constants.TALON_TIMEOUT_MS);
-        talonBL.config_kF(0, Constants.CLIMB_PIDFE[3], Constants.TALON_TIMEOUT_MS);
-
-        configMotorEncoder(talonFL, Constants.FRONT_LEFT_FORWARD_HALL_REVERSED, Constants.FRONT_LEFT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
-        configMotorEncoder(talonBR, Constants.BACK_RIGHT_FORWARD_HALL_REVERSED, Constants.BACK_RIGHT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
-        configMotorEncoder(talonFR, Constants.FRONT_RIGHT_FORWARD_HALL_REVERSED, Constants.FRONT_RIGHT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
-        configMotorEncoder(talonBL, Constants.BACK_LEFT_FORWARD_HALL_REVERSED, Constants.BACK_LEFT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
+        configMotorSensors(talonFL, Constants.FRONT_LEFT_FORWARD_HALL_REVERSED, Constants.FRONT_LEFT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
+        configMotorSensors(talonFR, Constants.FRONT_RIGHT_FORWARD_HALL_REVERSED, Constants.FRONT_RIGHT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
+        configMotorSensors(talonBL, Constants.BACK_LEFT_FORWARD_HALL_REVERSED, Constants.BACK_LEFT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
+        configMotorSensors(talonBR, Constants.BACK_RIGHT_FORWARD_HALL_REVERSED, Constants.BACK_RIGHT_REVERSE_HALL_REVERSED, FeedbackDevice.CTRE_MagEncoder_Relative);
     }
 
     /**
@@ -144,7 +112,7 @@ public class Climb extends Subsystem {
      * @param backwardLSReversed is the forward limit switch reversed.
      * @param feedbackDevice     the encoder type connected to the motor controller
      */
-    private void configMotorEncoder(TalonSRX motorController, boolean forwardLSReversed, boolean backwardLSReversed, FeedbackDevice feedbackDevice) {
+    private void configMotorSensors(TalonSRX motorController, boolean forwardLSReversed, boolean backwardLSReversed, FeedbackDevice feedbackDevice) {
         motorController.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, Constants.TALON_TIMEOUT_MS);
         motorController.configForwardLimitSwitchSource(
                 LimitSwitchSource.FeedbackConnector,
@@ -159,6 +127,14 @@ public class Climb extends Subsystem {
         motorController.configSelectedFeedbackSensor(feedbackDevice, 0, Constants.TALON_TIMEOUT_MS);
     }
 
+    private void configMotorMovement(TalonSRX motorController, boolean motorInverted, NeutralMode neutralMode, double[] pidfConstants, int timeout){
+        motorController.setInverted(motorInverted);
+        motorController.setNeutralMode(neutralMode); //what the motor does when not given voltage (Brake - decelerate the motor, Coast - not stop the motor)
+        motorController.config_kP(0, pidfConstants[0], timeout);
+        motorController.config_kI(0, pidfConstants[1], timeout);
+        motorController.config_kD(0, pidfConstants[2], timeout);
+        motorController.config_kF(0, pidfConstants[3], timeout);
+    }
     /**
      * auxiliary method used to make the code more understandable.
      *
