ffd9af6b585e510bf42633bc730ffebb396bbb84
lior <lior5858@gmail.com>
2019-02-13 10:53:52 +0200

    Update motors IDs

--- src/main/java/robot/subsystems/cargoIntake/Ports.java
+++ src/main/java/robot/subsystems/cargoIntake/Ports.java
@@ -1,8 +1,8 @@
 package robot.subsystems.cargoIntake;
 
 public class Ports {
-    final static int IntakeMotor = -1;
-    final static int WristMotor = -1;
+    final static int IntakeMotor = 10;
+    final static int WristMotor = 9;
     final static int proximitySensor = 0;
 
 }
