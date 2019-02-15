5fd3dcd7479974278885492ee5e8d06a8c98a488
Unknown <orel.david1502@gmail.com>
2019-02-15 11:47:47 +0200

    Set basic variables
    and that in the init the robot will start driving to the rocket

--- src/main/java/robot/subsystems/drivetrain/commands/RocketToLoading.java
+++ src/main/java/robot/subsystems/drivetrain/commands/RocketToLoading.java
@@ -1,36 +1,56 @@
 package robot.subsystems.drivetrain.commands;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+import robot.subsystems.drivetrain.pure_pursuit.*;
 
 /**
  *
  */
 public class RocketToLoading extends Command {
 
+    NetworkTableEntry distanceEntry;
+    NetworkTableEntry angleEntry;
+
     public RocketToLoading() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
     }
+        // Called just before this Command runs the first time
+        protected void initialize() {
+            NetworkTableInstance inst = NetworkTableInstance.getDefault();
+            NetworkTable table = inst.getTable("vision");
+            angleEntry = table.getEntry("angle");
+            distanceEntry = table.getEntry("distance");
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+            Path path = new Path();
+            path.appendWaypoint(new Waypoint(0,-3.5));
+            path.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
+            PurePursue pursue = new PurePursue(path, Constants.LOOKAHEAD_DISTANCE, Constants.kP, Constants.kA, Constants.kV, true, false);
+            pursue.start();
+        }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+        // Called repeatedly when this Command is scheduled to run
+        protected void execute() {
+                }
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+        // Make this return true when this Command no longer needs to run execute()
+        protected boolean isFinished() {
+            return Point.distance(Robot.drivetrain.currentLocation, target(angleEntry.getDouble(0), distanceEntry.getDouble(0)))
+                    <= 0;
+        }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+        // Called once after isFinished returns true
+        protected void end() {
+        }
+
+        // Called when another command which requires one or more of the same
+        // subsystems is scheduled to run
+        protected void interrupted() {
+        }
 }
