323846597ac19f868e5d37654137ab751385f463
Paulo Khayat <paulokhayat@gmail.com>
2019-02-15 12:11:10 +0200

    Rename constants for clarity

--- src/main/java/robot/subsystems/cargoIntake/Constants.java
+++ src/main/java/robot/subsystems/cargoIntake/Constants.java
@@ -1,7 +1,7 @@
 package robot.subsystems.cargoIntake;
 
 public class Constants {
-    public final static double GRIPPER_SPEED = 0.75;
+    public final static double GRIPPER_WHEELS_SPEED = 0.75;
     public final static double INITIAL_ANGLE = 0;//initial angle of the wrist
     public final static double INTAKE_ANGLE = 0;
     public final static double FOLDED_ANGLE = 165;//folded angle represents the angle in which the wrist is folded back inside the robot (number felt cute might delete later)
