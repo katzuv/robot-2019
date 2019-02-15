df5dff689dd5b5abe38af6e4091ef2d69c58aa08
orel-david <orel.david1502@gmail.com>
2019-02-15 16:52:32 +0200

    Create an instant command to enter the robot into the vision range

---
+++ src/main/java/robot/subsystems/drivetrain/Paths/HabToFarRocket.java
@@ -0,0 +1,46 @@
+package robot.subsystems.drivetrain.Paths;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import robot.subsystems.drivetrain.pure_pursuit.Constants;
+import robot.subsystems.drivetrain.pure_pursuit.Path;
+import robot.subsystems.drivetrain.pure_pursuit.PurePursue;
+import robot.subsystems.drivetrain.pure_pursuit.Waypoint;
+
+/**
+ *
+ */
+public class HabToFarRocket extends InstantCommand {
+
+    public HabToFarRocket() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        Path path = new Path();
+        path.appendWaypoint(new Waypoint(0.35, 5));
+        path.appendWaypoint(new Waypoint(0.75, 5.5));
+        path.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
+        PurePursue pursue = new PurePursue(path, Constants.LOOKAHEAD_DISTANCE, Constants.kP, Constants.kA, Constants.kV, true, false);
+        pursue.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
