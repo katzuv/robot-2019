eed0c47b8af1e81d9b42312998383ae524fe7e83
Paulo Khayat <paulokhayat@gmail.com>
2019-02-16 18:27:29 +0200

    Set constants to package private

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -5,18 +5,18 @@ public class Constants {
     public static final double MAX_RATE = 0.3;
     public static final double DISTANCE_PER_PULSE = (0.2032 * Math.PI) / 231;//diameter of the wheel is 0.2032 meters (8 inches), the encoder sends 226 pulses every 360 degree turn
     public static final double ROTATION_TOLERANCE = 0.05;
-    public static final int MOTION_MAGIC_CRUISE_VELOCITY = 372;
-    public static final int MOTION_MAGIC_ACCELERATION = 744;
-    public static final double PEAK_OUTPUT_FORWARD = 1;
-    public static final double PEAK_OUTPUT_REVERSE = -1;
-    public static final double NOMINAL_OUTPUT_FORWARD = 0;
-    public static final double NOMINAL_OUTPUT_REVERSE = 0;
-    public static final boolean LEFT_ENCODER_REVERSED = false;
-    public static final boolean RIGHT_ENCODER_REVERSED = false;
-    public static final double TICKS_PER_METER = 2138.7750882690398; // [1m / (diameter=0.1254 * pi)] * (ticks_per_meter=1024s)
-    public static final int TALON_RUNNING_TIMEOUT_MS = 0;
-    public static final int TALON_TIMEOUT_MS = 10;
-    public static final double[] PIDF = {0, 0, 0, 1023.0 / 744};
+    static final int MOTION_MAGIC_CRUISE_VELOCITY = 372;
+    static final int MOTION_MAGIC_ACCELERATION = 744;
+    static final double PEAK_OUTPUT_FORWARD = 1;
+    static final double PEAK_OUTPUT_REVERSE = -1;
+    static final double NOMINAL_OUTPUT_FORWARD = 0;
+    static final double NOMINAL_OUTPUT_REVERSE = 0;
+    static final boolean LEFT_ENCODER_REVERSED = false;
+    static final boolean RIGHT_ENCODER_REVERSED = false;
+    static final double TICKS_PER_METER = 2138.7750882690398; // [1m / (diameter=0.1254 * pi)] * (ticks_per_meter=1024s)
+    static final int TALON_RUNNING_TIMEOUT_MS = 0;
+    static final int TALON_TIMEOUT_MS = 10;
+    static final double[] PIDF = {0, 0, 0, 1023.0 / 744};
     // (8 inches), the encoder sends 226
     // pulses every 360 degree turn
     static final boolean LEFT_MASTER_REVERSED = false;
