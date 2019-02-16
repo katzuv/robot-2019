866d342ae02bfbd75bed5767dc97fc58ed2f87c4
lior <lior5858@gmail.com>
2019-02-16 14:57:36 +0200

    Adjust constants

--- src/main/java/robot/subsystems/cargoIntake/Constants.java
+++ src/main/java/robot/subsystems/cargoIntake/Constants.java
@@ -6,14 +6,16 @@ public class Constants {
     public final static double INTAKE_ANGLE = 0;
     public final static double FOLDED_ANGLE = 165;//folded angle represents the angle in which the wrist is folded back inside the robot (number felt cute might delete later)
 
-    public static final double MAX_VELOCITY = 2732; //currently in encoder units.
-    public static final double MAX_ACCEL = 1366;
+    public static final int MOTION_MAGIC_ACCELERATION = 1000;
+    public static final int CRUISE_VELOCITY = 1600;
 
-    final static double kP = 3;
-    final static double kI = 0;
-    final static double kD = 120;
-    final static double kF = 0.3744509516837482;
+    final static double kP = 0.6;
+    final static double kD = 100;
+    final static double kF = 0.48;
+
     final static int IZone = 50;
+    final static double kI = 0.001;
+
 
     final static double TICKS_PER_DEGREE = 11.73333333333333333333333*4; // (reduction=66/16) * (ticks_per_revolution=1024) / 360deg
 
