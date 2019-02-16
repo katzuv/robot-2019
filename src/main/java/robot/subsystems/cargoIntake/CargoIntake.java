27a5c34a0bf639f5d9a9b02771d629e6fff60dd9
lior <lior5858@gmail.com>
2019-02-16 12:45:38 +0200

    Increase peakOutput to 0.5

--- src/main/java/robot/subsystems/cargoIntake/CargoIntake.java
+++ src/main/java/robot/subsystems/cargoIntake/CargoIntake.java
@@ -52,12 +52,12 @@ public class CargoIntake extends Subsystem {
          */
         wrist.configNominalOutputForward(0.1, Constants.TALON_TIME_OUT);
         wrist.configNominalOutputReverse(-0.1, Constants.TALON_TIME_OUT);
-        wrist.configPeakOutputForward(0.3, Constants.TALON_TIME_OUT);
-        wrist.configPeakOutputReverse(-0.3, Constants.TALON_TIME_OUT);
+        wrist.configPeakOutputForward(0.5, Constants.TALON_TIME_OUT);
+        wrist.configPeakOutputReverse(-0.5, Constants.TALON_TIME_OUT);
         /*
         profile config
          */
-        wrist.selectProfileSlot(0,0);
+        wrist.selectProfileSlot(0, 0);
         /*
         motion magic speed config
          */
