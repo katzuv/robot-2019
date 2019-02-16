62173e044b3eaf42ccbb1bdefc01755d6eec42f6
Unknown <orel.david1502@gmail.com>
2019-02-16 13:13:18 +0200

    Create sub path from loading to farRocket
    this sub path will bring the robot into position where the far rocket in his range of view.

---
+++ src/main/java/robot/subsystems/drivetrain/Paths/Subpaths/LoadingToFarRocket.java
@@ -0,0 +1,57 @@
+package robot.subsystems.drivetrain.Paths.Subpaths;
+
+import edu.wpi.first.wpilibj.command.Command;
+import robot.subsystems.drivetrain.pure_pursuit.Constants;
+import robot.subsystems.drivetrain.pure_pursuit.Path;
+import robot.subsystems.drivetrain.pure_pursuit.PurePursue;
+import robot.subsystems.drivetrain.pure_pursuit.Waypoint;
+
+/**
+ *
+ */
+public class LoadingToFarRocket extends Command {
+
+    boolean isToRight;
+    int direction;// right positive left negative.
+
+
+
+    public LoadingToFarRocket(boolean isToRight) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+
+        this.isToRight = isToRight;
+        if (isToRight) {
+            direction = 1;
+        } else {
+            direction = -1;
+        }
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        Path path = new Path(new Waypoint[]{new Waypoint(direction * 0.25, 3.2), (new Waypoint(direction * 0.5, 5.5)),
+                (new Waypoint(direction*0.25,5))});
+        path.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
+        PurePursue pursue = new PurePursue(path, Constants.LOOKAHEAD_DISTANCE, Constants.kP, Constants.kA, Constants.kV, true, false);
+        pursue.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
