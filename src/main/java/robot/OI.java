2c1e737800730022caf154e259df0a82bfb3b6f6
Paulo Khayat <paulokhayat@gmail.com>
2019-02-17 14:04:52 +0200

    Fix comments

--- src/main/java/robot/OI.java
+++ src/main/java/robot/OI.java
@@ -55,8 +55,7 @@ public class OI {
     public OI (){
     }
     //// CREATING BUTTONS
-    // One type of butto][\
-    // n is a joystick button which is any button on a
+    // One type of button is a joystick button which is any button on a
     //// joystick.
     // You create one by telling it which joystick it's on and which button
     // number it is.
