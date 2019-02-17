f17a3a8ea10995beece0cbd4a94486fbc1d52367
Paulo Khayat <paulokhayat@gmail.com>
2019-02-18 01:21:34 +0200

    Add cancel() to interrupted

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/PurePursue.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/PurePursue.java
@@ -92,6 +92,8 @@ public class PurePursue extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
+        cancel();
     }
 
     /**
