da918f49fb50ef013fdc64f3e0c21acbf5c0deb7
Paulo Khayat <paulokhayat@gmail.com>
2019-02-17 13:09:03 +0200

    Remove prints

--- src/main/java/robot/subsystems/cargoIntake/commands/WristTurn.java
+++ src/main/java/robot/subsystems/cargoIntake/commands/WristTurn.java
@@ -24,15 +24,12 @@ public class WristTurn extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        SmartDashboard.putString("command state", "start");
         cargoIntake.setWristAngle(angle);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         cargoIntake.setWristAngle(angle);
-        SmartDashboard.putString("command state", "executing");
-
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -42,11 +39,6 @@ public class WristTurn extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-//        if (cargoIntake.getWristAngle() < 15)
-//            cargoIntake.wrist.set(ControlMode.MotionMagic, 0);
-
-        SmartDashboard.putString("command state", "end");
-        System.out.println("Wrist command ended, reached degrees " + angle + "talon " + cargoIntake.getWristAngle());
     }
 
     // Called when another command which requires one or more of the same
