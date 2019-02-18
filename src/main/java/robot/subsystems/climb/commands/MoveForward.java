fb4dcfb3a96019e8de1692d3428a00ada39139c9
Paulo Khayat <paulokhayat@gmail.com>
2019-02-18 17:14:24 +0200

    Finish moveForward command

--- src/main/java/robot/subsystems/climb/commands/MoveForward.java
+++ src/main/java/robot/subsystems/climb/commands/MoveForward.java
@@ -1,12 +1,16 @@
 package robot.subsystems.climb.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
 
 /**
  *
  */
 public class MoveForward extends Command {
-
+    private double velocity;
+    private double timeout;
+    private Timer timer = new Timer();
     /**
      * Drive the climb wheel forward at a specified speed, until an end statement is met.
      *
@@ -14,11 +18,15 @@ public class MoveForward extends Command {
      * @param timeout  time in seconds until the command stops
      */
     public MoveForward(double velocity, double timeout) {
-
+        this.velocity = velocity;
+        this.timeout = timeout;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.climb.setWheelVelocity(velocity);
+        timer.reset();
+        timer.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -27,11 +35,12 @@ public class MoveForward extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return timer.get() >= timeout;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+        Robot.climb.setWheelVelocity(0);
     }
 
     // Called when another command which requires one or more of the same
