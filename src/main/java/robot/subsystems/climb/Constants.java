af4067bdb8e1989ab9a932b0d6c706a8d72f7164
Paulo Khayat <paulokhayat@gmail.com>
2019-02-18 22:33:02 +0200

    Update climbing constants

--- src/main/java/robot/subsystems/climb/Constants.java
+++ src/main/java/robot/subsystems/climb/Constants.java
@@ -11,15 +11,20 @@ public class Constants {
     public static final double LEVEL_THREE_LEG_LENGTH = 0.5; //Length of leg in meters when climbing. todo:change
     public static final double LEVEL_TWO_LEG_LENGTH = 0.2; //Length of leg in meters when climbing to hab 2. todo:change
     public static final double CLIMB_TOLERANCE = 0.025; //tolerance in meters from the target position, to allow when stopping the code.
-    static final double TICKS_PER_METER = 57142; //Should take into account spiral pitch and diameter. todo:change
+    static final double TICKS_PER_METER = 28571; //Should take into account spiral pitch and diameter. todo:change
     static final int TALON_TIMEOUT_MS = 20;
     static final double[] CLIMB_PIDFE = {1, 0, 0, 1, 0}; //Proportional, Intergral, Differential, Feedforward and our own ErrorFix
 
     //Reverse motor directions
-    static final boolean FRONT_LEFT_TALON_REVERSE = false;
-    static final boolean FRONT_RIGHT_TALON_REVERSE = false;
-    static final boolean BACK_LEFT_TALON_REVERSE = false;
-    static final boolean BACK_RIGHT_TALON_REVERSE = false;
+    static final boolean FRONT_LEFT_TALON_REVERSE = true;
+    static final boolean FRONT_RIGHT_TALON_REVERSE = true;
+    static final boolean BACK_LEFT_TALON_REVERSE = true;
+    static final boolean BACK_RIGHT_TALON_REVERSE = true;
+
+    static final boolean FRONT_LEFT_ENCODER_REVERSE = true;
+    static final boolean FRONT_RIGHT_ENCODER_REVERSE = true;
+    static final boolean BACK_LEFT_ENCODER_REVERSE = true;
+    static final boolean BACK_RIGHT_ENCODER_REVERSE = true;
 
     //reverse hall effects on their respective motor. (normally_open = false, normally_closed = true)
     static final boolean FRONT_LEFT_FORWARD_HALL_REVERSED = true;
