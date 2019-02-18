c7fa3f6ce96cc227171adf20d111a3be9b07e02a
lior <lior5858@gmail.com>
2019-02-18 20:15:09 +0200

    Update CAN ID of Drivetrain's left master

--- src/main/java/robot/subsystems/drivetrain/Ports.java
+++ src/main/java/robot/subsystems/drivetrain/Ports.java
@@ -1,7 +1,7 @@
 package robot.subsystems.drivetrain;
 
 public class Ports {
-    final static int leftMaster = 12;
+    final static int leftMaster = 3;
     final static int leftSlave1 = 4;
     public static int leftSlave2 = 5;
     final static int rightMaster = 6;
