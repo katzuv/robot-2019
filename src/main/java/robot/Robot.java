8ebf5fd3272b1668f98cb2347af6c31235ba265a
lior <lior5858@gmail.com>
2019-02-19 16:20:25 +0200

    Change default robot to B

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -45,7 +45,7 @@ public class Robot extends TimedRobot {
 
 
     public static OI m_oi;
-    public final static boolean isRobotA = true;
+    public final static boolean isRobotA = false;
 
     Command m_autonomousCommand;
     SendableChooser<Command> m_chooser = new SendableChooser<>();
