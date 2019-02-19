051201ebec85111113c201470484bef2244a8073
orel-david <orel.david1502@gmail.com>
2019-02-19 12:48:26 +0200

    Add elevator and hatch intake command into the path of both rocket side

--- src/main/java/robot/subsystems/drivetrain/paths/FarRocketNearRocketAuto.java
+++ src/main/java/robot/subsystems/drivetrain/paths/FarRocketNearRocketAuto.java
@@ -5,6 +5,9 @@ import robot.subsystems.drivetrain.paths.subpaths.DriveToRocket;
 import robot.subsystems.drivetrain.paths.subpaths.FarRocketToHab;
 import robot.subsystems.drivetrain.paths.subpaths.HabToFarRocket;
 import robot.subsystems.drivetrain.paths.subpaths.RocketToLoading;
+import robot.subsystems.elevator.Constants;
+import robot.subsystems.elevator.commands.ElevatorCommand;
+import robot.subsystems.hatch_intake.commands.Gripper;
 
 /**
  *
@@ -13,10 +16,16 @@ public class FarRocketNearRocketAuto extends CommandGroup {
 
     public FarRocketNearRocketAuto() {
         addSequential(new HabToFarRocket(true));// drive to far rocket
+        addParallel(new ElevatorCommand(Constants.ELEVATOR_STATES.LEVEL1_HATCH));
         addSequential(new DriveToRocket());// drive to rocket when in his range of view
+        addSequential(new Gripper(false));
         addSequential(new FarRocketToHab());// drive from far rocket to loading
         addSequential(new RocketToLoading(false));//drive to loading when in his range of view
+        addParallel(new ElevatorCommand(Constants.ELEVATOR_STATES.LOADING_STATION));
+        addSequential(new Gripper(true));
         addSequential(new RocketToLoading(true));//drive to close rocket from loading
+        addSequential(new ElevatorCommand(Constants.ELEVATOR_STATES.LEVEL1_HATCH));
+        addSequential(new Gripper(false));
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
