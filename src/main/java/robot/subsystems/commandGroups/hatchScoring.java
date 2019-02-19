f31f12ccd901d664cf0d69e04a0079d13c3c1f66
orel-david <orel.david1502@gmail.com>
2019-02-19 14:33:51 +0200

    Create scoring function for hatch
    the elevator would rise to the desired level and then place the hatch

--- src/main/java/robot/subsystems/commandGroups/hatchScoring.java
+++ src/main/java/robot/subsystems/commandGroups/hatchScoring.java
@@ -1,13 +1,33 @@
 package robot.subsystems.commandGroups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import robot.subsystems.elevator.Constants;
+import robot.subsystems.elevator.commands.ElevatorCommand;
+import robot.subsystems.hatch_intake.commands.PlaceHatch;
 
 /**
  *
  */
 public class hatchScoring extends CommandGroup {
 
-    public hatchScoring() {
+    int level;
+
+    public hatchScoring(int level) {
+        this.level = level;
+        switch (level) {
+            case 1:
+                addSequential(new ElevatorCommand(Constants.ELEVATOR_STATES.LEVEL1_HATCH));
+                break;
+            case 2:
+                addSequential(new ElevatorCommand(Constants.ELEVATOR_STATES.LEVEL2_HATCH));
+                break;
+
+            case 3:
+                addSequential(new ElevatorCommand(Constants.ELEVATOR_STATES.LEVEL3_HATCH));
+                break;
+        }
+        addSequential(new PlaceHatch());
+
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
