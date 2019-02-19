70895f963c8bd8704c7374b5ab19aec48a694695
Paulo Khayat <paulokhayat@gmail.com>
2019-02-19 13:56:33 +0200

    TEMP steal joystick buttons for climbing command

--- src/main/java/robot/OI.java
+++ src/main/java/robot/OI.java
@@ -83,8 +83,8 @@ public class OI {
         rb.whileHeld(new GripperControl(0.9));
         start.whileHeld(new GripperControl(-0.9));
 
-        a.whenPressed(new WristTurn(Constants.WRIST_ANGLES.INITIAL));
-        b.whenPressed(new WristTurn(Constants.WRIST_ANGLES.UP));
+        a.whenPressed(new RiseToHeightEncoders(0.1));
+        b.whenPressed(new RiseToHeightEncoders(0));
         x.whenPressed(new WristTurn(Constants.WRIST_ANGLES.INTAKE));
         y.whenPressed(new WristTurn(Constants.WRIST_ANGLES.SHOOTING));
 
