97c5b42c72573a3ac93d8d33836fd8ebd09dedc9
lior <lior5858@gmail.com>
2019-02-19 15:34:56 +0200

    Rename cargoScoring command group

--- src/main/java/robot/subsystems/commandGroups/cargoScoring.java
+++ src/main/java/robot/subsystems/commandGroups/cargoScoring.java
@@ -10,9 +10,9 @@ import robot.subsystems.hatch_intake.commands.Gripper;
 /**
  *
  */
-public class cargoScore extends CommandGroup {
+public class cargoScoring extends CommandGroup {
 
-    public cargoScore(robot.subsystems.elevator.Constants.ELEVATOR_STATES state) {
+    public cargoScoring(robot.subsystems.elevator.Constants.ELEVATOR_STATES state) {
         addParallel(new ElevatorCommand(state.getLevelHeight()));
         addSequential(new WristTurn(Constants.WRIST_ANGLES.SHOOTING));
         addSequential(new GripperControl(Constants.GRIPPER_SHOOT_SPEED));
