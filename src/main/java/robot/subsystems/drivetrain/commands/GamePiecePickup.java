58ee2539cd0840149131b890bcef7244876548a0
lior <lior5858@gmail.com>
2019-02-19 23:30:47 +0200

    Add driving target to choose the target

--- src/main/java/robot/subsystems/drivetrain/commands/GamePiecePickup.java
+++ src/main/java/robot/subsystems/drivetrain/commands/GamePiecePickup.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import robot.Robot;
 import robot.subsystems.drivetrain.pure_pursuit.*;
 
-
 import static robot.Robot.*;
 
 
@@ -14,12 +13,7 @@ import static robot.Robot.*;
  *
  */
 public class GamePiecePickup extends Command {
-
-    public GamePiecePickup() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
-
+    private robot.subsystems.drivetrain.Constants.DRIVING_TARGETS CurrentTarget;
     private NetworkTableEntry CargotargetAngleEntry;
     private NetworkTableEntry CargotargetDistanceEntry;
     private NetworkTableEntry ReflectorAngleEntry;
@@ -27,8 +21,12 @@ public class GamePiecePickup extends Command {
     private NetworkTableEntry ReflectorFieldAngleEntry;
     private NetworkTableEntry HatchAngleEntry;
     private NetworkTableEntry HatchDistanceEntry;
-
-
+    public GamePiecePickup(robot.subsystems.drivetrain.Constants.DRIVING_TARGETS target) {
+        requires(drivetrain);
+        CurrentTarget = target;
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
 
     // Called just before this Command runs the first time
     protected void initialize() {
@@ -42,18 +40,30 @@ public class GamePiecePickup extends Command {
         HatchDistanceEntry = Robot.visionTable.getEntry("hatch_distance");
         ReflectorFieldAngleEntry = Robot.visionTable.getEntry("tape_field_angle");
         ReflectorAngleEntry = Robot.visionTable.getEntry("tape_angle");
-        ReflectorDistanceEntry = Robot.visionTable.getEntry("tape_distance");  
+        ReflectorDistanceEntry = Robot.visionTable.getEntry("tape_distance");
+        double targetDistance = 0;
+        double targetAngle = 0;
+        double targetFieldAngle = 0;
+        if (CurrentTarget == robot.subsystems.drivetrain.Constants.DRIVING_TARGETS.CARGO) {
+            targetDistance = CargotargetDistanceEntry.getDouble(0);
+            targetAngle = CargotargetAngleEntry.getDouble(0);
+        } else if (CurrentTarget == robot.subsystems.drivetrain.Constants.DRIVING_TARGETS.HATCH) {
+            targetDistance = HatchDistanceEntry.getDouble(0);
+            targetAngle = HatchAngleEntry.getDouble(0);
+        } else {
+            targetDistance = ReflectorDistanceEntry.getDouble(0);
+            targetAngle = ReflectorAngleEntry.getDouble(0);
+            targetFieldAngle = ReflectorFieldAngleEntry.getDouble(0);
+        }
 
-        double targetDistance = CargotargetDistanceEntry.getDouble(0)/100;
-        double targetAngle = CargotargetAngleEntry.getDouble(0);
-        Waypoint target = new Waypoint(Math.sin(Math.toRadians(targetAngle)) * targetDistance +0.15, Math.cos(Math.toRadians(targetAngle)) * targetDistance   );
-        Waypoint middleWP = new Waypoint(0, target.getY()-target.getY()/2);
-        Path path1 = new Path(new Waypoint[]{new Waypoint(0,0), middleWP ,  target});
+        Waypoint target = new Waypoint(Math.sin(Math.toRadians(targetAngle)) * targetDistance + 0.15, Math.cos(Math.toRadians(targetAngle)) * targetDistance);
+        Waypoint middleWP = new Waypoint(0, target.getY() - target.getY() / 2);
+        Path path1 = new Path(new Waypoint[]{new Waypoint(0, 0), middleWP, target});
         System.out.println(path1);
         path1.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
         SmartDashboard.putNumber("target distance", targetDistance);
         SmartDashboard.putNumber("target angle ", targetAngle);
-        path1 = new Path(new Point(0,0),0 , target, ReflectorFieldAngleEntry.getDouble(0),  Constants.TURN_RADIUS);
+        path1 = new Path(new Point(0, 0), 0, target, targetFieldAngle, Constants.TURN_RADIUS);
         path1.generateAll(Constants.WEIGHT_DATA, Constants.WEIGHT_SMOOTH, Constants.TOLERANCE, Constants.MAX_ACCEL, Constants.MAX_PATH_VELOCITY);
         PurePursue pursue = new PurePursue(path1, Constants.LOOKAHEAD_DISTANCE, Constants.kP, Constants.kA, Constants.kV, true, false);
         pursue.start();
