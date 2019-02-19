9fc0091d27a578faf5d8d97bffd5ebeab61178ec
orel-david <orel.david1502@gmail.com>
2019-02-19 12:51:52 +0200

    Add elevator and hatch intake command into the path of the cargo

--- src/main/java/robot/subsystems/drivetrain/paths/NearCargoAuto.java
+++ src/main/java/robot/subsystems/drivetrain/paths/NearCargoAuto.java
@@ -3,11 +3,20 @@ package robot.subsystems.drivetrain.paths;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import robot.subsystems.drivetrain.paths.subpaths.CargoToLoading;
 import robot.subsystems.drivetrain.paths.subpaths.HabToCargo;
+import robot.subsystems.elevator.Constants;
+import robot.subsystems.elevator.commands.ElevatorCommand;
+import robot.subsystems.hatch_intake.commands.Gripper;
 
 public class NearCargoAuto extends CommandGroup {
     public NearCargoAuto() {
         addSequential(new HabToCargo());//drive to cargo
+        addParallel(new ElevatorCommand(Constants.ELEVATOR_STATES.SHIP_HATCH));
+        addSequential(new Gripper(false));
         addSequential(new CargoToLoading());//drive to loading
+        addParallel(new ElevatorCommand(Constants.ELEVATOR_STATES.LOADING_STATION));
+        addSequential(new Gripper(true));
         addSequential(new CargoToLoading());//drive to the other cargo
+        addParallel(new ElevatorCommand(Constants.ELEVATOR_STATES.SHIP_HATCH));
+        addSequential(new Gripper(false));
     }
 }
