d80519a0bdbcf99f2eca3fa8e8ae7e5fedf1aeb1
LIor <lior5858@gmail.com>
2019-02-20 05:56:24 +0200

    Revert "Fix pneu ports for hatch intake for robot b"
    This reverts commit 6d46fa4c08284fe13b4e3b49d5f230d95c048ade.

--- src/main/java/robot/subsystems/hatch_intake/Ports.java
+++ src/main/java/robot/subsystems/hatch_intake/Ports.java
@@ -3,9 +3,9 @@ package robot.subsystems.hatch_intake;
 import static robot.Robot.isRobotA;
 
 public class Ports {
-    public final static int gripperReverse = isRobotA ? 2 : 1;
-    public final static int gripperForward = isRobotA ? 3 : 2;
-    public final static int gripperPlateReverse = isRobotA ? 1 : 0;
-    public final static int gripperPlateForward = isRobotA ? 0 : 3;
+    public final static int gripperReverse = isRobotA ? 2 : 2;
+    public final static int gripperForward = isRobotA ? 3 : 3;
+    public final static int gripperPlateReverse = isRobotA ? 1 : 1;
+    public final static int gripperPlateForward = isRobotA ? 0 : 0;
     public final static int proximitySensor = 7;
 }
