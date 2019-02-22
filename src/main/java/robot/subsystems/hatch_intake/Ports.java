a4d732740ec56427880d1f48b84e71d925bbd945
Paulo Khayat <paulokhayat@gmail.com>
2019-02-22 10:59:33 +0200

    Fix hatch ports

--- src/main/java/robot/subsystems/hatch_intake/Ports.java
+++ src/main/java/robot/subsystems/hatch_intake/Ports.java
@@ -3,8 +3,8 @@ package robot.subsystems.hatch_intake;
 import static robot.Robot.isRobotA;
 
 public class Ports {
-    public final static int gripperReverse = isRobotA ? 2 : 2;
-    public final static int gripperForward = isRobotA ? 3 : 3;
+    public final static int gripperReverse = isRobotA ? 3 : 2;
+    public final static int gripperForward = isRobotA ? 2 : 3;
     public final static int gripperPlateReverse = isRobotA ? 1 : 1;
     public final static int gripperPlateForward = isRobotA ? 0 : 0;
     public final static int proximitySensor = 7;
