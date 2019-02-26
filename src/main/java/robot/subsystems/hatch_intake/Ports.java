409e95f919f720c164c29ae69f661da4040634ea
LIor <lior5858@gmail.com>
2019-02-26 15:08:27 +0200

    Auto stash before merge of "command-groups" and "master"

--- src/main/java/robot/subsystems/hatch_intake/Ports.java
+++ src/main/java/robot/subsystems/hatch_intake/Ports.java
@@ -3,9 +3,16 @@ package robot.subsystems.hatch_intake;
 import static robot.Robot.isRobotA;
 
 public class Ports {
+<<<<<<< Updated upstream
     public final static int gripperReverse = isRobotA ? 3 : 2;
     public final static int gripperForward = isRobotA ? 2 : 1;
     public final static int gripperPlateReverse = isRobotA ? 1 : 3;
     public final static int gripperPlateForward = isRobotA ? 0 : 0;
+=======
+    public final static int gripperReverse = 3;
+    public final static int gripperForward = 2;
+    public final static int gripperPlateReverse = 1;
+    public final static int gripperPlateForward = 0;
+>>>>>>> Stashed changes
     public final static int proximitySensor = 7;
 }
