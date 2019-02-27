7c9e2d2d9b33d5b5a44a74e5bc7052ed04c40ea2
LIor <lior5858@gmail.com>
2019-02-27 18:20:44 +0200

    Tune Pure Pursuit constants

--- src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/pure_pursuit/Constants.java
@@ -3,7 +3,7 @@ package robot.subsystems.drivetrain.pure_pursuit;
 public class Constants {
     //DRIVING CONSTANTS
 
-    public final static double LOOKAHEAD_DISTANCE = 0.65; //in meters
+    public final static double LOOKAHEAD_DISTANCE = 0.7; //in meters
 
     public static final double MAX_VELOCITY = 3.762;
 
@@ -16,9 +16,9 @@ public class Constants {
     public final static double TOLERANCE = 0.001;
 
     //VELOCITY CONSTANTS
-    public static final double MAX_PATH_VELOCITY = 1.2;
-    public static final double MAX_ACCEL = 0.5;
-    public static final double K_CURVE = 3; //number from 1 to 5
+    public static final double MAX_PATH_VELOCITY = 1.75;
+    public static final double MAX_ACCEL = 0.4;  //0.7
+    public static final double K_CURVE = 0.8; //number from 1 to 5
 
     //DRIVING CONSTANTS (pure numbers)
     public final static double kV = 1 / MAX_VELOCITY;
@@ -28,7 +28,7 @@ public class Constants {
     //Pure pursuit real time
     public final static double MIN_DISTANCE = 0.5;//The minimum distance between the robot to the target point
 
-    public final static double STOP_SPEED_THRESH = 0.1; //the speed the robot could stop at the end of the path.
+    public final static double STOP_SPEED_THRESH = 0.05;     //the speed the robot could stop at the end of the path.
 
     public final static double ROBOT_WIDTH = 0.74; //width of the robot
 
@@ -36,6 +36,6 @@ public class Constants {
     public final static double TURN_RADIUS = 0.09;//turn radius for dubins path
     public static final double RADIUS_CLOSING = 0.05;
     public static final double START_SPACE = 0.25;
-    public static final double END_SPACE = 0.25;
+    public static final double END_SPACE = 0.4;
 
 }
