237496fd9330dcf543477aaf58794cc831d96164
Paulo Khayat <paulokhayat@gmail.com>
2019-02-28 21:26:03 +0200

    Add command to OI

--- src/main/java/robot/OI.java
+++ src/main/java/robot/OI.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 import robot.subsystems.climb.Climb;
+import robot.subsystems.climb.commands.CalibrateLegs;
 import robot.subsystems.climb.commands.RiseToHeight;
 import robot.subsystems.climb.commands.RiseToHeightEncoders;
 
@@ -94,7 +95,7 @@ public class OI {
         a.whenPressed(new RiseToHeightEncoders(Climb.HAB_LEG_HEIGHTS.LEVEL2));
         b.whenPressed(new RiseToHeightEncoders(0));
         //x.whenPressed(new WristTurn(Constants.WRIST_ANGLES.INTAKE));
-        y.whenPressed(new WristTurn(Constants.WRIST_ANGLES.SHOOTING));
+        y.whenPressed(new CalibrateLegs());
 
         select.whenPressed(new GripperTransportation());
         lb.whenPressed(new Gripper());
