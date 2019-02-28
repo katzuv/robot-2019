86385355068610667cc657dbc1ad04cc81430491
Paulo Khayat <paulokhayat@gmail.com>
2019-02-28 21:25:45 +0200

    Add isFinished to command

--- src/main/java/robot/subsystems/climb/commands/CalibrateLegs.java
+++ src/main/java/robot/subsystems/climb/commands/CalibrateLegs.java
@@ -31,7 +31,7 @@ public class CalibrateLegs extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return climb.areAllLegsUp();
     }
 
     // Called once after isFinished returns true
