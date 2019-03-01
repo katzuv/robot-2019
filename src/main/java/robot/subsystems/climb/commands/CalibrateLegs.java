932fd72cb40464e90fb723fd6aa33d2ad893430f
Paulo Khayat <paulokhayat@gmail.com>
2019-03-01 20:17:07 +0200

    Reset encoders at end of command

--- src/main/java/robot/subsystems/climb/commands/CalibrateLegs.java
+++ src/main/java/robot/subsystems/climb/commands/CalibrateLegs.java
@@ -36,6 +36,7 @@ public class CalibrateLegs extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+        climb.resetEncoders();
     }
 
     // Called when another command which requires one or more of the same
