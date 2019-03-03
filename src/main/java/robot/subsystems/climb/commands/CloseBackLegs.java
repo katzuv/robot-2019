b4e4b75d91ce97f1ee58a18030e3d3059e1fe0c4
Paulo Khayat <paulokhayat@gmail.com>
2019-03-03 11:28:41 +0200

    Make half of close back legs

--- src/main/java/robot/subsystems/climb/commands/CloseBackLegs.java
+++ src/main/java/robot/subsystems/climb/commands/CloseBackLegs.java
@@ -1,6 +1,11 @@
 package robot.subsystems.climb.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+import robot.subsystems.climb.Constants;
+import robot.subsystems.climb.TiltUtils;
+
+import static robot.Robot.climb;
 
 /**
  *
@@ -8,12 +13,17 @@ import edu.wpi.first.wpilibj.command.Command;
 public class CloseBackLegs extends Command {
 
     public CloseBackLegs() {
+        requires(climb);
+
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+       climb.setLegBLHeight(0,0);
+       climb.setLegBRHeight(0,0);
+
     }
 
     // Called repeatedly when this Command is scheduled to run
