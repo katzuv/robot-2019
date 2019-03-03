54b4c333dbcfc5ef0bdd2acbed09c6711e905633
Paulo Khayat <paulokhayat@gmail.com>
2019-03-03 13:33:56 +0200

    Create close forward legs

--- src/main/java/robot/subsystems/climb/commands/CloseForwardLegs.java
+++ src/main/java/robot/subsystems/climb/commands/CloseForwardLegs.java
@@ -1,6 +1,11 @@
 package robot.subsystems.climb.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+import robot.subsystems.climb.Constants;
+import robot.subsystems.climb.TiltUtils;
+
+import static robot.Robot.climb;
 
 /**
  *
@@ -8,12 +13,17 @@ import edu.wpi.first.wpilibj.command.Command;
 public class CloseForwardLegs extends Command {
 
     public CloseForwardLegs() {
+        requires(climb);
+
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        climb.setLegFLHeight(0,0);
+        climb.setLegFRHeight(0,0);
+
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -22,11 +32,14 @@ public class CloseForwardLegs extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return  climb.getLegFLHeight() < Constants.CLIMB_TOLERANCE &&
+                climb.getLegFRHeight() < Constants.CLIMB_TOLERANCE;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+        climb.setLegFLSpeed(0);
+        climb.setLegFRSpeed(0);
     }
 
     // Called when another command which requires one or more of the same
