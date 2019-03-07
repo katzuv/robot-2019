c3f56fe85a92c1c0e2721bdec9c6c13fc7b0bef4
Paulo Khayat <paulokhayat@gmail.com>
2019-03-07 21:00:43 +0200

    Raise Kbeta to 20?

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -37,7 +37,7 @@ public class Constants {
      */
     public static final double[] PIDF = isRobotA ? new double[]{1.85, 0.002, 10, 1.45} : new double[]{0.9, 0, 10, 1.8};
     public static final double kZeta = 0.7;
-    public static final double kBeta = 15;
+    public static final double kBeta = 20;
     public static final double radiusFromEnd = 1;
     public static final double angleKp = 0.2;
     public static final List<TimingConstraint<Pose2dWithCurvature>> constraints = new ArrayList<>();
