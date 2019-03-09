717417014bab95d0ed6164dfc261dcf328215eac
Paulo Khayat <paulokhayat@gmail.com>
2019-03-09 16:38:39 +0200

    Little things

--- src/main/java/robot/OI.java
+++ src/main/java/robot/OI.java
@@ -192,10 +192,6 @@ public class OI {
         return xbox.getRawButton(9);
     }
 
-    public boolean autoShift() {
-        return xbox.getStartButton();
-    }
-
     private TimedTrajectory<Pose2dWithCurvature> generateLoadingStationTrajectory() {
         List<TimingConstraint<Pose2dWithCurvature>> constraints = new ArrayList<>();
         constraints.add(new CentripetalAccelerationConstraint(AccelerationKt.getAcceleration(LengthKt.getMeter(1.2192))));
@@ -217,33 +213,4 @@ public class OI {
                         true
                 );
     }
-
-    // CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a
-    //// joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-
-
-    // Start the command when the button is released and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
 }
