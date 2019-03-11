ec81f3f809bbd2ea34d29a2cf2c8e476c55f6734
Paulo Khayat <paulokhayat@gmail.com>
2019-03-11 19:35:11 +0200

    Fix build error at afc30b in unused command
    i promise ill try to stop

--- src/main/java/robot/subsystems/climb/commands/RiseToHeightCombined.java
+++ src/main/java/robot/subsystems/climb/commands/RiseToHeightCombined.java
@@ -31,20 +31,22 @@ public class RiseToHeightCombined extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        double minimumLeg = Math.min(climb.getLegBLHeight(), Math.min(climb.getLegBRHeight(), Math.min(climb.getLegFLHeight(), climb.getLegFRHeight())));
-        climb.setLegFLHeight(targetHeight, minimumLeg - climb.getLegFLHeight(), TiltUtils.getLegLength(-Constants.LEG_X_DIMENSION, Constants.LEG_Y_DIMENSION, Robot.navx.getPitch(), Robot.navx.getRoll()));
-        climb.setLegFRHeight(targetHeight, minimumLeg - climb.getLegFRHeight(), TiltUtils.getLegLength(Constants.LEG_X_DIMENSION, Constants.LEG_Y_DIMENSION, Robot.navx.getPitch(), Robot.navx.getRoll()));
-        climb.setLegBLHeight(targetHeight, minimumLeg - climb.getLegBLHeight(), TiltUtils.getLegLength(-Constants.BACK_LEG_X_DIMENSION, -Constants.BACK_LEG_Y_DIMENSION, Robot.navx.getPitch(), Robot.navx.getRoll()));
-        climb.setLegBRHeight(targetHeight, minimumLeg - climb.getLegBRHeight(), TiltUtils.getLegLength(Constants.BACK_LEG_X_DIMENSION, -Constants.BACK_LEG_Y_DIMENSION, Robot.navx.getPitch(), Robot.navx.getRoll()));
+//        double minimumLeg = Math.min(climb.getLegBLHeight(), Math.min(climb.getLegBRHeight(), Math.min(climb.getLegFLHeight(), climb.getLegFRHeight())));
+//        climb.setLegFLHeight(targetHeight, minimumLeg - climb.getLegFLHeight(), TiltUtils.getLegLength(-Constants.LEG_X_DIMENSION, Constants.LEG_Y_DIMENSION, Robot.navx.getPitch(), Robot.navx.getRoll()));
+//        climb.setLegFRHeight(targetHeight, minimumLeg - climb.getLegFRHeight(), TiltUtils.getLegLength(Constants.LEG_X_DIMENSION, Constants.LEG_Y_DIMENSION, Robot.navx.getPitch(), Robot.navx.getRoll()));
+//        climb.setLegBLHeight(targetHeight, minimumLeg - climb.getLegBLHeight(), TiltUtils.getLegLength(-Constants.BACK_LEG_X_DIMENSION, -Constants.BACK_LEG_Y_DIMENSION, Robot.navx.getPitch(), Robot.navx.getRoll()));
+//        climb.setLegBRHeight(targetHeight, minimumLeg - climb.getLegBRHeight(), TiltUtils.getLegLength(Constants.BACK_LEG_X_DIMENSION, -Constants.BACK_LEG_Y_DIMENSION, Robot.navx.getPitch(), Robot.navx.getRoll()));
 
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Math.abs(climb.getLegFLHeight() - targetHeight) < Constants.CLIMB_TOLERANCE &&
-                Math.abs(climb.getLegFRHeight() - targetHeight) < Constants.CLIMB_TOLERANCE &&
-                Math.abs(climb.getLegBLHeight() - targetHeight) < Constants.CLIMB_TOLERANCE &&
-                Math.abs(climb.getLegBRHeight() - targetHeight) < Constants.CLIMB_TOLERANCE;
+//        return Math.abs(climb.getLegFLHeight() - targetHeight) < Constants.CLIMB_TOLERANCE &&
+//                Math.abs(climb.getLegFRHeight() - targetHeight) < Constants.CLIMB_TOLERANCE &&
+//                Math.abs(climb.getLegBLHeight() - targetHeight) < Constants.CLIMB_TOLERANCE &&
+//                Math.abs(climb.getLegBRHeight() - targetHeight) < Constants.CLIMB_TOLERANCE;
+
+        return true;
     }
 
     // Called once after isFinished returns true
