53f46b83939445914c959124200c359bafce8ab1
Paulo Khayat <paulokhayat@gmail.com>
2019-03-12 15:32:15 +0200

    Remove height appropriation on legs
    for some reason, after calibration only the front legs go up. no idea why

--- src/main/java/robot/subsystems/climb/commands/RiseToHeightEncoders.java
+++ src/main/java/robot/subsystems/climb/commands/RiseToHeightEncoders.java
@@ -32,10 +32,9 @@ public class RiseToHeightEncoders extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        double minimumLeg = Math.min(climb.getLegBLHeight(), Math.min(climb.getLegBRHeight(), Math.min(climb.getLegFLHeight(), climb.getLegFRHeight()))); //TODO: only rise, not go down?
-        climb.setLegDriveHeight(targetHeight, (minimumLeg-climb.getLegFRHeight())); //TODO: should be get drive height
-        climb.setLegBLHeight(targetHeight+0.01, minimumLeg - climb.getLegBLHeight());
-        climb.setLegBRHeight(targetHeight+0.01, minimumLeg - climb.getLegBRHeight());
+        climb.setLegDriveHeight(targetHeight, 0); //TODO: should be get drive height
+        climb.setLegBLHeight(targetHeight+0.01, 0);
+        climb.setLegBRHeight(targetHeight+0.01, 0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
