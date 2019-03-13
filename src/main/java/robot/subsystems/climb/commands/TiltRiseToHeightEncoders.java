9134497ae49474b63306f93018c3bc3d9324f36c
Paulo Khayat <paulokhayat@gmail.com>
2019-03-13 06:30:43 +0200

    Rename tilt by encoders

--- src/main/java/robot/subsystems/climb/commands/TiltRiseToHeightEncoders.java
+++ src/main/java/robot/subsystems/climb/commands/TiltRiseToHeightEncoders.java
@@ -6,14 +6,14 @@ import robot.subsystems.climb.Climb;
 /**
  * Essentially the same as RiseToHeight, but makes sure the back legs start more out, to prevent tipping.
  */
-public class TiltRiseToHeight extends CommandGroup {
+public class TiltRiseToHeightEncoders extends CommandGroup {
 
-    public TiltRiseToHeight(double targetHeight) {
+    public TiltRiseToHeightEncoders(double targetHeight) {
         addSequential(new MoveBackLegs(0.0125));
         addSequential(new RiseToHeightEncoders(targetHeight));
     }
 
-    public TiltRiseToHeight(Climb.HAB_LEG_HEIGHTS height_state){
+    public TiltRiseToHeightEncoders(Climb.HAB_LEG_HEIGHTS height_state){
         this(height_state.getHABHHeight());
     }
 }
