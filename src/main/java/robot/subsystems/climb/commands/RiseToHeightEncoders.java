8f067b239a1afc4bb359a264e841192e8e15b829
LIor <lior5858@gmail.com>
2019-03-13 10:16:16 +0200

    Dont cancel command on interrupt

--- src/main/java/robot/subsystems/climb/commands/RiseToHeightEncoders.java
+++ src/main/java/robot/subsystems/climb/commands/RiseToHeightEncoders.java
@@ -56,6 +56,6 @@ public class RiseToHeightEncoders extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-        cancel();
+        end();
     }
 }
