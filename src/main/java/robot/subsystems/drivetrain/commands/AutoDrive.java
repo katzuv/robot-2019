1a11f042ee3d94bab029028cae2d6515c9216462
LIor <lior5858@gmail.com>
2019-03-19 18:53:57 +0200

    Split vision and end state to two commands

--- src/main/java/robot/subsystems/drivetrain/commands/AutoDrive.java
+++ src/main/java/robot/subsystems/drivetrain/commands/AutoDrive.java
@@ -14,64 +14,46 @@ import static robot.Robot.drivetrain;
  *
  */
 public class AutoDrive extends Command {
-    private MiniPID speedPid = new MiniPID(Constants.PIDVisionSpeed[0], Constants.PIDVisionSpeed[1], Constants.PIDVisionSpeed[2]); //TODO: move these to constants class
+    private MiniPID speedPid = new MiniPID(Constants.PIDVisionSpeed[0], Constants.PIDVisionSpeed[1], Constants.PIDVisionSpeed[2]);
     private MiniPID turnPid = new MiniPID(Constants.PIDVisionTurn[0], Constants.PIDVisionTurn[1], Constants.PIDVisionTurn[2]);
 
     private NetworkTableEntry angleEntry = Robot.visionTable.getEntry("tape_angle");
     private NetworkTableEntry distanceEntry = Robot.visionTable.getEntry("tape_distance");
 
-    private boolean finish = false;
-    private Timer timer = new Timer();
-    private double lastDistance = 0;
-    private boolean driven = false;
-
+    private double visionAngle;
+    private double visionDistance;
     public AutoDrive() {
         speedPid.setOutputLimits(-Constants.PEAK_VISION_SPEED, Constants.PEAK_VISION_SPEED);
         requires(drivetrain);
     }
 
     protected void initialize() {
-        finish = false;
-        driven = false;
-        timer.reset();
     }
 
     protected void execute() {
-        double angle = angleEntry.getDouble(0);
-        double distance = distanceEntry.getDouble(0);
-        if (distance < 0.4) { //Stops in cases where the target is too close, or when the target isn't visible(in these cases the distance is 0)
-            finish = true;
-        }
-        double speed = speedPid.getOutput(distance, 0.6);
-        double turn = turnPid.getOutput(angle, 0);
-        if (finish) {
-            if (!driven) {
-                drivetrain.driveDistance(lastDistance - 0.3);
-                driven = true;
-            }
-        } else {
-            if (angle > 1) {
-                turn -= 0.05;
-            } else if (angle < -1) {
-                turn += 0.05;
-            }
-            drivetrain.setSpeed(speed - turn, speed + turn);
-            lastDistance = distance;
+        visionAngle = angleEntry.getDouble(0);
+        visionDistance = distanceEntry.getDouble(0);
 
-            SmartDashboard.putNumber("AutoDrive: Turn value", turn);
-            SmartDashboard.putNumber("AutoDrive: Speed", speed);
-        }
+        double speed = speedPid.getOutput(visionDistance, 0.6);
+        double turn = turnPid.getOutput(visionAngle, 0);
 
+        if (visionAngle > 1)
+            turn -= 0.05;
+        else if (visionAngle < -1)
+            turn += 0.05;
+
+        drivetrain.setSpeed(speed - turn, speed + turn);
+
+        SmartDashboard.putNumber("AutoDrive: Turn value", turn);
+        SmartDashboard.putNumber("AutoDrive: Speed", speed);
     }
 
 
     protected boolean isFinished() {
-        return false;
+        return visionDistance < 0.4;
     }
 
     protected void end() {
-        timer.stop();
-        drivetrain.setSpeed(0, 0);
     }
 
     protected void interrupted() {
---
+++ src/main/java/robot/subsystems/drivetrain/commands/DistanceDrive.java
@@ -0,0 +1,40 @@
+package robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import static robot.Robot.drivetrain;
+
+/**
+ *
+ */
+public class DistanceDrive extends Command {
+
+    public DistanceDrive() {
+        requires(drivetrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        drivetrain.driveDistance(0.1); //TODO: currently this doesn't use the real values
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        drivetrain.setSpeed(0,0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+}
