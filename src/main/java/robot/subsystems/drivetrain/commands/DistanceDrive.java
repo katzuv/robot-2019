e145265746b3ad4dc73c0fad77f811ee0b7aaa4b
LIor <lior5858@gmail.com>
2019-03-21 12:33:53 +0200

    Write basic end statement for driving forward

--- src/main/java/robot/subsystems/drivetrain/commands/DistanceDrive.java
+++ src/main/java/robot/subsystems/drivetrain/commands/DistanceDrive.java
@@ -1,6 +1,7 @@
 package robot.subsystems.drivetrain.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import robot.subsystems.drivetrain.Constants;
 
 import static robot.Robot.drivetrain;
 
@@ -9,6 +10,8 @@ import static robot.Robot.drivetrain;
  */
 public class DistanceDrive extends Command {
     private double distance;
+    private double startDistance;
+
     public DistanceDrive(double distance) {
         requires(drivetrain);
         this.distance = distance;
@@ -16,7 +19,9 @@ public class DistanceDrive extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        startDistance = (drivetrain.getLeftDistance() + drivetrain.getRightDistance()) / 2;
         drivetrain.driveDistance(distance); //TODO: currently this doesn't use the real values
+        drivetrain.setMotorsToBrake();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -25,12 +30,13 @@ public class DistanceDrive extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return (distance + startDistance) - ((drivetrain.getLeftDistance() + drivetrain.getRightDistance()) / 2) < Constants.ENDING_TOLERANCE;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-        drivetrain.setSpeed(0,0);
+        drivetrain.setSpeed(0, 0);
+
     }
 
     // Called when another command which requires one or more of the same
