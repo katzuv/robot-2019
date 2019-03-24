2ce1cd8c48916c96ec77a21e6b8a9d1efa07761a
LIor <lior5858@gmail.com>
2019-03-24 18:14:09 +0200

    Create vision placing command groups

---
+++ src/main/java/robot/subsystems/command_groups/VisionPlaceHatch.java
@@ -0,0 +1,18 @@
+package robot.subsystems.command_groups;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import robot.subsystems.drivetrain.commands.VisionDrive;
+import robot.subsystems.elevator.Constants;
+import robot.subsystems.elevator.commands.ElevatorCommand;
+
+/**
+ *
+ */
+public class VisionPlaceHatch extends CommandGroup {
+
+    public VisionPlaceHatch(Constants.ELEVATOR_STATES height) {
+        addParallel(new ElevatorCommand(height));
+        addSequential(new VisionDrive());
+        addSequential(new HatchScoring(height, false));
+    }
+}
