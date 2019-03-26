5ac510bd57cc2538834469774940736d131fa7a5
LIor <lior5858@gmail.com>
2019-03-26 18:24:01 +0200

    Improve turn angle

--- src/main/java/robot/subsystems/drivetrain/commands/TurnAngle.java
+++ src/main/java/robot/subsystems/drivetrain/commands/TurnAngle.java
@@ -10,33 +10,39 @@ import static robot.Robot.navx;
  */
 public class TurnAngle extends Command {
     private double angle;
+    private double setpoint;
+
     public TurnAngle(double angle) {
-        this.angle = navx.getAngle()+angle;
+        this.angle = angle;
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-
+        this.setpoint = navx.getAngle() + angle;
+        drivetrain.setMotorsToBrake();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        drivetrain.setVelocity(1.5, -1.5);
+        drivetrain.setVelocity(2.5, -2.5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return this.angle-navx.getAngle() < 10;
+        return this.setpoint - navx.getAngle() < 2;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+        drivetrain.setSpeed(0, 0);
+        drivetrain.setMotorsToCoast();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
