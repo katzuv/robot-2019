2e98a30e5085eaec20e976725bc6286213bc6e3f
LIor <lior5858@gmail.com>
2019-03-27 17:35:27 +0200

    Create utils class

---
+++ src/main/java/robot/utilities/Utils.java
@@ -0,0 +1,37 @@
+package robot.utilities;
+
+import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d;
+import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2dWithCurvature;
+import org.ghrobotics.lib.mathematics.twodim.trajectory.TrajectoryGeneratorKt;
+import org.ghrobotics.lib.mathematics.twodim.trajectory.types.TimedTrajectory;
+import org.ghrobotics.lib.mathematics.units.LengthKt;
+import org.ghrobotics.lib.mathematics.units.derivedunits.AccelerationKt;
+import org.ghrobotics.lib.mathematics.units.derivedunits.VelocityKt;
+import robot.subsystems.drivetrain.Constants;
+
+import java.util.List;
+
+public class Utils {
+
+    public static double map(double x, double in_min, double in_max, double out_min, double out_max) {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+    public static double constrainedMap(double x, double in_min, double in_max, double out_min, double out_max) {
+        return Math.max(out_min, Math.min(out_max, map(x, in_min, in_max, out_min, out_max)));
+    }
+
+    public static TimedTrajectory<Pose2dWithCurvature> generateTrajectory(List<Pose2d> points, double startingVelocity, double endingVelocity, boolean reversed) {
+        return TrajectoryGeneratorKt.getDefaultTrajectoryGenerator()
+                .generateTrajectory(
+                        points,
+                        Constants.constraints,
+                        VelocityKt.getVelocity(LengthKt.getMeter(startingVelocity)),
+                        VelocityKt.getVelocity(LengthKt.getMeter(endingVelocity)),
+                        VelocityKt.getVelocity(LengthKt.getMeter(Constants.RAMSETE_PEAK_VELOCITY)),
+                        AccelerationKt.getAcceleration(LengthKt.getMeter(Constants.RAMSETE_PEAK_ACCELERATION)),
+                        reversed,
+                        true
+                );
+    }
+}
