850995b8a79724c68c07dec04249de4f5f502fbb
LIor <lior5858@gmail.com>
2019-03-27 17:37:46 +0200

    Implement utils generation

--- src/main/java/robot/subsystems/drivetrain/commands/TurnAngle.java
+++ src/main/java/robot/subsystems/drivetrain/commands/TurnAngle.java
@@ -1,6 +1,7 @@
 package robot.subsystems.drivetrain.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import robot.utilities.Utils;
 
 import static robot.Robot.drivetrain;
 import static robot.Robot.navx;
@@ -26,7 +27,7 @@ public class TurnAngle extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        double velocity = constrainedMap(Math.abs(this.setpoint - navx.getAngle()), 0, angle, 0.3, 2.5); //Have the value start at 2.5 and end at a slower speed
+        double velocity = Utils.constrainedMap(Math.abs(this.setpoint - navx.getAngle()), 0, angle, 0.3, 2.5); //Have the value start at 2.5 and end at a slower speed
         drivetrain.setVelocity(velocity, -velocity);
     }
 
@@ -47,11 +48,4 @@ public class TurnAngle extends Command {
         end();
     }
 
-    private double map(double x, double in_min, double in_max, double out_min, double out_max){
-        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
-
-    private double constrainedMap(double x, double in_min, double in_max, double out_min, double out_max){
-        return Math.max(out_min, Math.min(out_max, map(x, in_min, in_max, out_min, out_max)));
-    }
 }
