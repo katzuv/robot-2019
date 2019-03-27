00b7529e25656a995ee2c869dcd78fb627b4dc60
LIor <lior5858@gmail.com>
2019-03-27 17:49:55 +0200

    Slightly change PID values and velocities on constraints

--- src/main/java/robot/subsystems/drivetrain/Constants.java
+++ src/main/java/robot/subsystems/drivetrain/Constants.java
@@ -16,8 +16,6 @@ import static robot.Robot.isRobotA;
 
 public class Constants {
     public final static double ROBOT_WIDTH = 0.74; //the distance between the left and right wheels on the robot
-    public static final double MAX_RATE = isRobotA ? 0.3 : 0.3;
-    public static final double DISTANCE_PER_PULSE = isRobotA ? (0.2032 * Math.PI) / 231 : (0.2032 * Math.PI) / 231;  //diameter of the wheel is 0.2032 meters (8 inches), the encoder sends 226 pulses every 360 degree turn
     public static final boolean LEFT_ENCODER_REVERSED = isRobotA ? false : false;
     public static final boolean RIGHT_ENCODER_REVERSED = isRobotA ? false : false;
     public static final double LEFT_TICKS_PER_METER = isRobotA ? 2138.7750882690398 : 2138.7750882690398; // measured average distance (robot a), [1m / (diameter=0.1524 * pi)] * (ticks_per_meter=1024s) (robot b)
@@ -42,12 +40,12 @@ public class Constants {
 
     public static final double SLOW_JOYSTICK_SPEED = 0.9; //multiplied by joystick value, keep at 1 for no changes.
 
-    public static double[] PIDVisionTurn = isRobotA ? new double[]{0.02, 0.0006, 0.1} : new double[]{0, 0, 0};
+    public static double[] PIDVisionTurn = isRobotA ? new double[]{0.02, 0.0006, 0.1} : new double[]{0.02, 0.0006, 0.1};
 
     /*
     Ramsete constants
      */
-    public static final double[] PIDFLeft = isRobotA ? new double[]{1.5, 0.002, 10, 1.3} : new double[]{1.5, 0, 10, 1.3};
+    public static final double[] PIDFLeft = isRobotA ? new double[]{1.5, 0, 10, 1.3} : new double[]{1.5, 0, 10, 1.3};
     public static final double[] PIDFRight = isRobotA ? new double[]{2, 0.007, 20, 1.45} : new double[]{1.5, 0, 10, 1.3};
     public static final double kZeta = 0.8;
     public static final double kBeta = isRobotA ? 2 : 2;
@@ -57,7 +55,7 @@ public class Constants {
     public static final List<TimingConstraint<Pose2dWithCurvature>> constraints = new ArrayList<>();
 
     static {
-        constraints.add(new CentripetalAccelerationConstraint(AccelerationKt.getAcceleration(LengthKt.getMeter(1.5))));
+        constraints.add(new CentripetalAccelerationConstraint(AccelerationKt.getAcceleration(LengthKt.getMeter(1.7))));
         constraints.add(new VelocityLimitRegionConstraint(new Rectangle2d(LengthKt.getFeet(4), LengthKt.getFeet(7), LengthKt.getFeet(8), LengthKt.getFeet(20)), VelocityKt.getVelocity(LengthKt.getFeet(3))));
     }
 }
