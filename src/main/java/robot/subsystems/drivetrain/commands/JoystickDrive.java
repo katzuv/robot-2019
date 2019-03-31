3d713827931f4d50f88f4662cb2aa4ff78f832fd
Paulo Khayat <paulokhayat@gmail.com>
2019-03-31 20:02:19 +0300

    Move around wrist speeds when climbing

--- src/main/java/robot/subsystems/drivetrain/commands/JoystickDrive.java
+++ src/main/java/robot/subsystems/drivetrain/commands/JoystickDrive.java
@@ -71,7 +71,7 @@ public class JoystickDrive extends Command {
         } //TODO: put this in subsystem
         if(climb.isClimbing()) //maybe also lower speeds when back legs are lowered.
         {
-            gripperWheels.setGripperSpeed(rightOutput*0.8); //TODO: not good, no requires
+            gripperWheels.setGripperSpeed(Math.min(0.9,Math.max(0,rightOutput))); //TODO: not good, no requires
             climb.setWheelSpeed(rightOutput*0.8);
             drivetrain.setSpeed(rightOutput/Constants.DRIVE_CLIMB_DRIVETRAIN_DIVISOR+ OI.rightStick.getX()*0.1,
                     rightOutput/Constants.DRIVE_CLIMB_DRIVETRAIN_DIVISOR- OI.rightStick.getX()*0.1);
