9f89e4baab04529eec3f21213558e68aaac7515a
Paulo Khayat <paulokhayat@gmail.com>
2019-04-12 16:03:00 +0300

    Fix hatch scoring command group

--- src/main/java/robot/subsystems/command_groups/HatchScoring.java
+++ src/main/java/robot/subsystems/command_groups/HatchScoring.java
@@ -3,22 +3,23 @@ package robot.subsystems.command_groups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import robot.subsystems.elevator.Constants;
+import robot.subsystems.elevator.commands.ElevatorCommand;
+import robot.subsystems.hatch_intake.commands.Fangs;
+import robot.subsystems.hatch_intake.commands.Flower;
+import robot.subsystems.wrist_control.commands.WristTurn;
 
+/**
+ * Hatch scoring command group, new to the wrist hatch mechanism.
+ */
 public class HatchScoring extends CommandGroup {
-    
-
-    public HatchScoring(Constants.ELEVATOR_HEIGHTS height, boolean retract) {
-        addSequential(new PlaceHatch(height));
-
-        if (retract) {
-            addSequential(new WaitCommand(0.3));
-            addSequential(new RetractHatch());
-        }
+    public HatchScoring(double height) {
+        addSequential(new ElevatorCommand(height));
+        addSequential(new WristTurn(robot.subsystems.wrist_control.Constants.WRIST_ANGLES.FORWARD));
+        addSequential(new Flower(true));
+        addSequential(new Fangs(true,0.4));
     }
 
     public HatchScoring(Constants.ELEVATOR_HEIGHTS height) {
-        addSequential(new PlaceHatch(height));
-        addSequential(new WaitCommand(0.3));
-        addSequential(new RetractHatch());
+        this(height.getLevelHeight());
     }
 }
