49f8e8a029e46e8c9d3a0b7d823101b4a5741a13
Repulser <thetechproject2014@gmail.com>
2019-04-15 12:37:27 +0300

    Adjust command groups and add testing buttons

--- src/main/java/robot/subsystems/command_groups/VisionPlaceHatch.java
+++ src/main/java/robot/subsystems/command_groups/VisionPlaceHatch.java
@@ -1,9 +1,11 @@
 package robot.subsystems.command_groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 import robot.subsystems.drivetrain.commands.VisionDrive;
 import robot.subsystems.elevator.Constants;
 import robot.subsystems.elevator.commands.ElevatorCommand;
+import robot.subsystems.wrist_control.commands.WristTurn;
 
 /**
  *
@@ -11,8 +13,14 @@ import robot.subsystems.elevator.commands.ElevatorCommand;
 public class VisionPlaceHatch extends CommandGroup {
 
     public VisionPlaceHatch(Constants.ELEVATOR_HEIGHTS height) {
-        addParallel(new ElevatorCommand(height));
+        addSequential(new CommandGroup() {
+            {
+                addParallel(new ElevatorCommand(height));
+                addParallel(new WristTurn(robot.subsystems.wrist_control.Constants.WRIST_ANGLES.FORWARD));
+            }
+        });
         addSequential(new VisionDrive());
-//        addSequential(new HatchScoring(height, false));
+        addSequential(new WaitCommand(0.2));
+        addSequential(new HatchScoring(height));
     }
 }
