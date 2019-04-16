16cd4d0111a4ed262a55b56f32957911f58cae4e
LIor <lior5858@gmail.com>
2019-04-16 14:38:58 +0300

    Make VisionDrive a constants speed

--- src/main/java/robot/subsystems/drivetrain/commands/VisionDrive.java
+++ src/main/java/robot/subsystems/drivetrain/commands/VisionDrive.java
@@ -15,18 +15,15 @@ public class VisionDrive extends Command {
     /*
             For now have all the constants here for testing, when done move to constants file
          */
-    private double TARGET_VISION_DISTANCE = 1.05; //distance from the target to stop
-    private static final double EXIT_TOLERANCE = 0.23;
+    private double TARGET_VISION_DISTANCE = 1.2; //distance from the target to stop
+
     private MiniPID turnPid = new MiniPID(Constants.PIDVisionTurn[0], Constants.PIDVisionTurn[1], Constants.PIDVisionTurn[2]);
-    private MiniPID forwardPid = new MiniPID(Constants.PIDVisionForward[0], Constants.PIDVisionForward[1], Constants.PIDVisionForward[2]);
     private NetworkTableEntry angleEntry = Robot.visionTable.getEntry("tape_angle");
     private NetworkTableEntry distanceEntry = Robot.visionTable.getEntry("tape_distance");
     private NetworkTableEntry seenEntry = Robot.visionTable.getEntry("tape_seen");
 
     public VisionDrive() {
         turnPid.setOutputLimits(-0.5, 0.5);
-        forwardPid.setOutputLimits(-0.5, 0.5);
-        forwardPid.setDirection(true);
         requires(drivetrain);
     }
 
@@ -38,7 +35,7 @@ public class VisionDrive extends Command {
         double visionAngle = angleEntry.getDouble(0);
         double visionDistance = distanceEntry.getDouble(0);
 
-        double speed = forwardPid.getOutput(visionDistance, TARGET_VISION_DISTANCE);
+        double speed = Constants.VISION_SPEED;
         double turn = turnPid.getOutput(visionAngle, 0);
 
         if (visionAngle > 1) {
@@ -53,7 +50,7 @@ public class VisionDrive extends Command {
     }
 
     protected boolean isFinished() {
-        return !seenEntry.getBoolean(false) || distanceEntry.getDouble(0) < TARGET_VISION_DISTANCE + EXIT_TOLERANCE;
+        return !seenEntry.getBoolean(false) || distanceEntry.getDouble(0) < TARGET_VISION_DISTANCE;
     }
 
     protected void end() {
@@ -66,6 +63,5 @@ public class VisionDrive extends Command {
 
     public void updateConstants() {
         turnPid.setPID(Constants.PIDVisionTurn[0], Constants.PIDVisionTurn[1], Constants.PIDVisionTurn[2]);
-        forwardPid.setPID(Constants.PIDVisionForward[0], Constants.PIDVisionForward[1], Constants.PIDVisionForward[2]);
     }
 }
