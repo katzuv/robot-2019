e6fce25657c709849e2d0904e5e2ee1ba84f6cec
LIor <lior5858@gmail.com>
2019-04-17 17:21:57 +0300

    Load csv trajectories

--- src/main/java/robot/subsystems/drivetrain/talon_profiling/Profiles.java
+++ src/main/java/robot/subsystems/drivetrain/talon_profiling/Profiles.java
@@ -9,15 +9,22 @@ public class Profiles {
     public static BufferedTrajectoryPointStream toRocketLeft;
     public static BufferedTrajectoryPointStream toRocketRight;
 
-    public static BufferedTrajectoryPointStream toRocketLeft2;
-    public static BufferedTrajectoryPointStream toRocketRight2;
+    public static BufferedTrajectoryPointStream toFarRocketLeft;
+    public static BufferedTrajectoryPointStream toFarRocketRight;
+
+    public static BufferedTrajectoryPointStream farRocketToLoadingRevLeft;
+    public static BufferedTrajectoryPointStream farRocketToLoadingRevRight;
 
     static {
         toRocketLeft = Drivetrain.loadTrajectoryFromCSV("rocket_left.csv");
         toRocketRight = Drivetrain.loadTrajectoryFromCSV("rocket_right.csv");
 
-        toRocketLeft2 = Drivetrain.loadTrajectoryFromCSV("test6_left.csv");
-        toRocketRight2 = Drivetrain.loadTrajectoryFromCSV("test6_right.csv");
+        toFarRocketLeft = Drivetrain.loadTrajectoryFromCSV("far_rocket_p1_left.csv");
+        toFarRocketRight = Drivetrain.loadTrajectoryFromCSV("far_rocket_p1_right.csv");
+
+        farRocketToLoadingRevLeft = Drivetrain.loadTrajectoryFromCSV("far_rocket_loading_reverse_left.csv");
+        farRocketToLoadingRevRight = Drivetrain.loadTrajectoryFromCSV("far_rocket_loading_reverse_right.csv");
     }
+
 
 }
