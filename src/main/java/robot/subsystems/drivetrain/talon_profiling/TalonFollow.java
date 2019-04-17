a5cf2275cacb67deebec4485357ce480f6ad01a9
LIor <lior5858@gmail.com>
2019-04-17 12:01:29 +0300

    Implement motion following auto
    no explanation here, sorry.

--- src/main/java/robot/subsystems/drivetrain/talon_profiling/TalonFollow.java
+++ src/main/java/robot/subsystems/drivetrain/talon_profiling/TalonFollow.java
@@ -1,5 +1,6 @@
 package robot.subsystems.drivetrain.talon_profiling;
 
+import com.ctre.phoenix.motion.BufferedTrajectoryPointStream;
 import edu.wpi.first.wpilibj.command.Command;
 
 import static robot.Robot.drivetrain;
@@ -9,13 +10,18 @@ import static robot.Robot.drivetrain;
  */
 public class TalonFollow extends Command {
 
-    public TalonFollow() {
+    private final BufferedTrajectoryPointStream rightStream;
+    private final BufferedTrajectoryPointStream leftStream;
+
+    public TalonFollow(BufferedTrajectoryPointStream leftStream, BufferedTrajectoryPointStream rightStream) {
+        this.leftStream = leftStream;
+        this.rightStream = rightStream;
         requires(drivetrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        drivetrain.clearTrajectories();
+        drivetrain.startMotionProfile(leftStream, rightStream);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -24,15 +30,17 @@ public class TalonFollow extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return drivetrain.isMotionProfileDone();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+        drivetrain.setSpeed(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
