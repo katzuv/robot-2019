7eef6edb52d4d73e4726077a1e99de6f17a3421e
Paulo <paulokhayat@gmail.com>
2019-04-17 17:25:01 +0300

    Change flower ports

--- src/main/java/robot/subsystems/hatch_intake/Ports.java
+++ src/main/java/robot/subsystems/hatch_intake/Ports.java
@@ -3,7 +3,7 @@ package robot.subsystems.hatch_intake;
 import static robot.Robot.isRobotA;
 
 public class Ports {
-    public final static int flowerReverse = isRobotA ? 0 : 1;
+    public final static int flowerReverse = isRobotA ? 4 : 1;
     public final static int flowerForward = isRobotA ? 1 : 0;
     public final static int pusherReverse = isRobotA ? 3 : 3;
     public final static int pusherForward = isRobotA ? 2 : 2;
