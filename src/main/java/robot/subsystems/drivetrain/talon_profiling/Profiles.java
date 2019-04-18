5bc05ef6c7f13234e80a98e38ace354b47a41f45
Paulo <paulokhayat@gmail.com>
2019-04-18 11:07:58 +0300

    Remove far hatch and start near hatch auto

--- src/main/java/robot/subsystems/drivetrain/talon_profiling/Profiles.java
+++ src/main/java/robot/subsystems/drivetrain/talon_profiling/Profiles.java
@@ -6,24 +6,12 @@ import robot.subsystems.drivetrain.Drivetrain;
 
 public class Profiles {
 
-    public static BufferedTrajectoryPointStream toRocketLeft;
-    public static BufferedTrajectoryPointStream toRocketRight;
-
-    public static BufferedTrajectoryPointStream toFarRocketLeft;
-    public static BufferedTrajectoryPointStream toFarRocketRight;
-
-    public static BufferedTrajectoryPointStream farRocketToLoadingRevLeft;
-    public static BufferedTrajectoryPointStream farRocketToLoadingRevRight;
+    public static BufferedTrajectoryPointStream toNearRocketLeft;
+    public static BufferedTrajectoryPointStream toNearRocketRight;
 
     static {
-        toRocketLeft = Drivetrain.loadTrajectoryFromCSV("rocket_left.csv");
-        toRocketRight = Drivetrain.loadTrajectoryFromCSV("rocket_right.csv");
-
-        toFarRocketLeft = Drivetrain.loadTrajectoryFromCSV("far_rocket_p1_left.csv");
-        toFarRocketRight = Drivetrain.loadTrajectoryFromCSV("far_rocket_p1_right.csv");
-
-        farRocketToLoadingRevLeft = Drivetrain.loadTrajectoryFromCSV("far_rocket_loading_reverse_left.csv");
-        farRocketToLoadingRevRight = Drivetrain.loadTrajectoryFromCSV("far_rocket_loading_reverse_right.csv");
+        toNearRocketLeft = Drivetrain.loadTrajectoryFromCSV("near_rocket_left.csv");
+        toNearRocketRight = Drivetrain.loadTrajectoryFromCSV("near_rocket_right.csv");
     }
 
 
