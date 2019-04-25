bbeee275cedf83cab3808ae6dbdbb1e62b52ac3a
Paulo <paulokhayat@gmail.com>
2019-04-25 17:44:43 +0300

    Disable cargo button for drivers

--- src/main/java/robot/subsystems/command_groups/VisionPlaceCargo.java
+++ src/main/java/robot/subsystems/command_groups/VisionPlaceCargo.java
@@ -16,20 +16,20 @@ import robot.utilities.SetRocket;
 public class VisionPlaceCargo extends CommandGroup {
 
     public VisionPlaceCargo(int state) {
-        if(state == 0) {
-            addSequential(new SetRocket(true));
-            addSequential(new CargoScoring(state, false));
-            addSequential(new WaitCommand(0.2));
-            addSequential(new VisionDrive(1));
-            addSequential(new GripperControl(Constants.GRIPPER_SPEED.SHIP.getValue()), 0.3);
-        }
-        else {
-            addSequential(new SetRocket(false));
-            addSequential(new VisionDrive(robot.subsystems.drivetrain.Constants.CARGO_TARGET_DISTANCE));
-            addSequential(new CargoScoring(state, false));
-            addSequential(new WaitCommand(0.2));
-            addSequential(new CargoRubbing(false));
-        }
+//        if(state == 0) {
+//            addSequential(new SetRocket(true));
+//            addSequential(new CargoScoring(state, false));
+//            addSequential(new WaitCommand(0.2));
+//            addSequential(new VisionDrive(1));
+//            addSequential(new GripperControl(Constants.GRIPPER_SPEED.SHIP.getValue()), 0.3);
+//        }
+//        else {
+//            addSequential(new SetRocket(false));
+//            addSequential(new VisionDrive(robot.subsystems.drivetrain.Constants.CARGO_TARGET_DISTANCE));
+//            addSequential(new CargoScoring(state, false));
+//            addSequential(new WaitCommand(0.2));
+//            addSequential(new CargoRubbing(false));
+//        }
 
     }
 }
